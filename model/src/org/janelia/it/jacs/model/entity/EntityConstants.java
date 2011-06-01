69218ff0b24f6b0d9d70ecca775e4dc94b5954b7
Sean Murphy <murphys@janelia.hhmi.org>
2011-06-01 14:42:59 +0000

    updated filename

---
+++ model/src/org/janelia/it/jacs/model/entity/EntityConstants.java
@@ -0,0 +1,13 @@
+package org.janelia.it.jacs.model.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: murphys
+ * Date: 6/1/11
+ * Time: 9:58 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EntityConstants {
+
+    transient public final String FILE_PATH = "FILE_PATH";
+}
