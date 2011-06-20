a362008aad37f1125765a2dd765dd198dee2476a
Todd Safford <saffordt@janelia.hhmi.org>
2011-06-20 17:29:58 +0000

    Changes to make the data sources a little more generic

--- compute/src/org/janelia/it/jacs/compute/service/neuronSeparator/NeuronSeparationPipelineService.java
+++ compute/src/org/janelia/it/jacs/compute/service/neuronSeparator/NeuronSeparationPipelineService.java
@@ -81,8 +81,8 @@ public class NeuronSeparationPipelineService implements IService {
             // Run the Consolidator Tool...
             ColorSeparatorResultNode colorNode = (ColorSeparatorResultNode) EJBFactory.getRemoteComputeBean().getResultNodeByTaskId(colorSepTask.getObjectId());
             // todo this should be a separate process running on the grid
-            String cmdLine =
-                    "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64:/groups/scicomp/jacsData/servers/saffordt-ws1/executables/v3d/v3d_main/common_lib/lib/;"+
+            String cmdLine = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64:"+
+                            SystemConfigurationProperties.getString("Executables.ModuleBase")+"/v3d/v3d_main/common_lib/lib/;"+
                             SystemConfigurationProperties.getString("Executables.ModuleBase")+
                             "/v3d/v3d/v3d -cmd consolidate-color-separator-output -i "+
                             colorNode.getDirectoryPath()+" -o "+
