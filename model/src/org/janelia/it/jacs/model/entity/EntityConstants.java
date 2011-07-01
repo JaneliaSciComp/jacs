30b1313aab9ebaced3cafe40f1f236addaa1e3f7
Todd Safford <saffordt@janelia.hhmi.org>
2011-07-01 18:49:35 +0000

    Made the DynamicTree and Outlines more generic

--- model/src/org/janelia/it/jacs/model/entity/EntityConstants.java
+++ model/src/org/janelia/it/jacs/model/entity/EntityConstants.java
@@ -33,6 +33,7 @@ public class EntityConstants {
 
     public static final long TYPE_ONTOLOGY_ROOT_ID = 1616533546980081843L;
     public static final long TYPE_ONTOLOGY_ELEMENT_ID = 1616533546954916019L;
+    public static final long TYPE_FOLDER_ID = 1616540598318137523L;
 
     // Entity Data Attributes
     public static final String ATTRIBUTE_FILE_PATH          = "File Path";
