6a1cbb8cc230c72136549026310cb84c1ba91b3b
Konrad Rokicki <rokickik@janelia.hhmi.org>
2011-10-21 20:17:45 +0000

    request 5 grid slots due to significant memory usage other changes to bring this back up to speed

--- compute/src/org/janelia/it/jacs/compute/service/neuronSeparator/NeuronSeparationPipelineService.java
+++ compute/src/org/janelia/it/jacs/compute/service/neuronSeparator/NeuronSeparationPipelineService.java
@@ -5,10 +5,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import org.apache.log4j.Logger;
+import org.janelia.it.jacs.compute.drmaa.DrmaaHelper;
+import org.janelia.it.jacs.compute.drmaa.SerializableJobTemplate;
 import org.janelia.it.jacs.compute.engine.data.MissingDataException;
 import org.janelia.it.jacs.compute.engine.service.ServiceException;
 import org.janelia.it.jacs.compute.service.common.ProcessDataHelper;
 import org.janelia.it.jacs.compute.service.common.grid.submit.sge.SubmitDrmaaJobService;
+import org.janelia.it.jacs.compute.util.FileUtils;
 import org.janelia.it.jacs.model.tasks.neuronSeparator.NeuronSeparatorPipelineTask;
 import org.janelia.it.jacs.model.user_data.neuronSeparator.NeuronSeparatorResultNode;
 import org.janelia.it.jacs.model.vo.ParameterException;
@@ -21,8 +24,14 @@ import org.janelia.it.jacs.model.vo.ParameterException;
  */
 public class NeuronSeparationPipelineService extends SubmitDrmaaJobService {
 
+	// Reserve 5 out of the 6 slots on a node. This gives us 20 GB of memory, hopefully enough for one lousy neuron separation. 
+	private static final int NUM_GRID_SLOTS = 5;
+	
     private static final String CONFIG_PREFIX = "neuSepConfiguration.";
     private static final int TIMEOUT_SECONDS = 1800;  // 30 minutes
+    private static final String REMOTE_SCRIPT = "runsep.fedora.sh";
+    private static final String STDOUT_FILE = "runsep.out";
+    private static final String STDERR_FILE = "runsep.err";
 
     @Override
     protected String getGridServicePrefixName() {
@@ -43,20 +52,38 @@ public class NeuronSeparationPipelineService extends SubmitDrmaaJobService {
 
     private void createShellScript(FileWriter writer)
             throws IOException, ParameterException, MissingDataException, InterruptedException, ServiceException {
-        Logger logger = ProcessDataHelper.getLoggerForTask(processData, this.getClass());
-        NeuronSeparatorPipelineTask task = (NeuronSeparatorPipelineTask) ProcessDataHelper.getTask(processData);
-        NeuronSeparatorResultNode parentNode = (NeuronSeparatorResultNode) ProcessDataHelper.getResultFileNode(processData);
-
+    	
+    	NeuronSeparatorResultNode parentNode = (NeuronSeparatorResultNode) ProcessDataHelper.getResultFileNode(processData);
+    	Logger logger = ProcessDataHelper.getLoggerForTask(processData, this.getClass());
+        
         logger.info("Starting NeuronSeparationPipelineService with taskId=" + task.getObjectId() + " resultNodeId=" + parentNode.getObjectId() + " resultDir=" + parentNode.getDirectoryPath());
 
-        StringBuffer script = new StringBuffer();
-        script.append("set -o errexit\n");
-        script.append(NeuronSeparatorHelper.getNeuronSeparationCommands(task, parentNode, "mylib", "\n"));
-        logger.info("NeuronSeparationCommands=\n" + script);
-        
-        writer.write(script.toString());
+        String script = NeuronSeparatorHelper.getNeuronSeparationCommands((NeuronSeparatorPipelineTask)task, parentNode, "mylib.fedora", " ; ");
+    	File scriptFile = new File(parentNode.getDirectoryPath(), REMOTE_SCRIPT);
+    	FileUtils.writeStringToFile(scriptFile, script);
+
+    	File outFile = new File(parentNode.getDirectoryPath(), STDOUT_FILE);
+        File errFile = new File(parentNode.getDirectoryPath(), STDERR_FILE);
+        // Need to use bash to get process substitution for the tricksy tee stuff to work. It is explained here:
+        // http://stackoverflow.com/questions/692000/how-do-i-write-stderr-to-a-file-while-using-tee-with-a-pipe
+        // The reason we want both outputs is because we want to match the "runsep.out" contract of the other neuron
+        // separator services, but we also want to give the STDERR stream to Drmaa so that it knows about errors.
+        String cmdLine = "bash "+ scriptFile.getAbsolutePath() +
+        	" > >(tee "+outFile.getAbsolutePath()+") 2> >(tee "+errFile.getAbsolutePath()+" >&2)";
+
+        StringBuffer wrapper = new StringBuffer();
+        wrapper.append("set -o errexit\n");
+        wrapper.append(cmdLine).append("\n");
+        writer.write(wrapper.toString());
     }
 
+    @Override
+    protected SerializableJobTemplate prepareJobTemplate(DrmaaHelper drmaa) throws Exception {
+    	SerializableJobTemplate jt = super.prepareJobTemplate(drmaa);
+    	jt.setNativeSpecification("-pe batch "+NUM_GRID_SLOTS);
+    	return jt;
+    }
+    
     @Override
     public int getJobTimeoutSeconds() {
         return TIMEOUT_SECONDS;
