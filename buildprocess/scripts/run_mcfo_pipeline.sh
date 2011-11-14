fbbbf550722b546d7a526e4299974eaad31d8023
Konrad Rokicki <rokickik@janelia.hhmi.org>
2011-11-14 20:28:06 +0000

    better way to execute jmx methods

--- buildprocess/scripts/run_mcfo_pipeline.sh
+++ buildprocess/scripts/run_mcfo_pipeline.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-/usr/bin/wget -v --post-data="action=invokeOp&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodIndex=2&arg0=system&arg1=False" http://$1:8180/jmx-console/HtmlAdaptor
+#action=invokeOp&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodIndex=2&arg0=system&arg1=False" 
+/usr/bin/wget -v --post-data="action=invokeOpByName&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodName=runMCFODataPipelineService&argType=java.lang.String&arg0=system&argType=boolean&arg1=False" http://$1:8180/jmx-console/HtmlAdaptor
 
