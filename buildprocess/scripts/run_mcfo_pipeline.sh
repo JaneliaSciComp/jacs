99dad1d2191622d19c933bee1a40620abac612a8
Konrad Rokicki <rokickik@janelia.hhmi.org>
2011-11-14 18:37:04 +0000

    fixed line break

--- buildprocess/scripts/run_mcfo_pipeline.sh
+++ buildprocess/scripts/run_mcfo_pipeline.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-/usr/bin/wget -v --post-data="action=invokeOp&name=ComputeServer%3Aservice%3DWorkstationDataManager&metho
-dIndex=0&arg0=1675994300812361899" http://rokicki-ws.janelia.priv:8180/jmx-console/HtmlAdaptor
+/usr/bin/wget -v --post-data="action=invokeOp&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodIndex=0&arg0=1675994300812361899" http://rokicki-ws.janelia.priv:8180/jmx-console/HtmlAdaptor
 
