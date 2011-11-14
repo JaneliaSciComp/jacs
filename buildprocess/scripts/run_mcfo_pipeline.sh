1ada9ef43e2ab3af955bd534fb44a4ebd19889e2
Konrad Rokicki <rokickik@janelia.hhmi.org>
2011-11-14 18:35:22 +0000

    fixed hostname

--- buildprocess/scripts/run_mcfo_pipeline.sh
+++ buildprocess/scripts/run_mcfo_pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget -q --post-data="action=invokeOp&name=ComputeServer%3Aservice%3DWorkstationDataManager&metho
-dIndex=0&arg0=1675994300812361899" http://rokicki-ws:8180/jmx-console/HtmlAdaptor
+/usr/bin/wget -v --post-data="action=invokeOp&name=ComputeServer%3Aservice%3DWorkstationDataManager&metho
+dIndex=0&arg0=1675994300812361899" http://rokicki-ws.janelia.priv:8180/jmx-console/HtmlAdaptor
 
