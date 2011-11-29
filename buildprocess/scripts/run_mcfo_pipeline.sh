4364e81f9879204c9620828bf95970d3e4534f10
Konrad Rokicki <rokickik@janelia.hhmi.org>
2011-11-29 16:25:25 +0000

    updated to run new method

--- buildprocess/scripts/run_mcfo_pipeline.sh
+++ buildprocess/scripts/run_mcfo_pipeline.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-#action=invokeOp&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodIndex=2&arg0=system&arg1=False" 
-/usr/bin/wget -v --post-data="action=invokeOpByName&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodName=runMCFODataPipelineService&argType=java.lang.String&arg0=system&argType=boolean&arg1=False" http://$1:8180/jmx-console/HtmlAdaptor
+/usr/bin/wget -v --post-data="action=invokeOpByName&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodName=runMCFODataPipeline&argType=java.lang.String&arg0=system&argType=boolean&arg1=False&argType=java.lang.String&arg2=/groups/flylight/flylight/flip/SecData/tiles&argType=java.lang.String&arg3=FlyLight+Single+Neuron+Data" http://$1:8180/jmx-console/HtmlAdaptor
 
