df1d849b8f2849f0217ec31fce751a16a1255d7d
Konrad Rokicki <rokickik@janelia.hhmi.org>
2011-12-09 15:45:30 +0000

    added script for creating code freezes fixed some bugs with deployment

--- buildprocess/scripts/hudson/build_jacs_part3_linux.sh
+++ buildprocess/scripts/hudson/build_jacs_part3_linux.sh
@@ -8,8 +8,8 @@
 
 INSTALL_VAA3D=1
 INSTALL_NEUSEP=1
-INSTALL_JACS=1
-INSTALL_JACSDATA=1
+INSTALL_SERVER=1
+INSTALL_CLIENT=1
 
 # Exit after any error
 set -o errexit
@@ -28,6 +28,7 @@ JACS_COMPILE_DIR="$COMPILE_DIR/jacs_FlySuite_${FWVER}"
 
 INSTALL_DIR="$EXE_DIR/install"
 VAA3D_INSTALL_REDHAT_DIR="$INSTALL_DIR/vaa3d_FlySuite_${FWVER}-redhat"
+VAA3D_INSTALL_FEDORA_DIR="$INSTALL_DIR/vaa3d_FlySuite_${FWVER}-fedora"
 NEUSEP_INSTALL_REDHAT_DIR="$INSTALL_DIR/neusep_FlySuite_${FWVER}-redhat"
 VAA3D_INSTALL_SYMLINK="$INSTALL_DIR/vaa3d-redhat"
 NEUSEP_INSTALL_SYMLINK="$INSTALL_DIR/neusep-redhat"
@@ -45,14 +46,16 @@ echo "Installing FlySuite version $FWVER (Part 3)"
 ################################################################
 if [ $INSTALL_VAA3D == 1 ]; then
     echo "Installing Vaa3D in $VAA3D_INSTALL_REDHAT_DIR"
-    mkdir -p $VAA3D_INSTALL_REDHAT_DIR
-    cp -R VAA3D_COMPILE_REDHAT_DIR/v3d/* $VAA3D_INSTALL_REDHAT_DIR
+    rm -rf $VAA3D_INSTALL_REDHAT_DIR || true
+    cp -R $VAA3D_COMPILE_REDHAT_DIR/v3d $VAA3D_INSTALL_REDHAT_DIR
 
-    echo "Installing Vaa3D in $VAA3D_COMPILE_FEDORA_DIR"
-    mkdir -p $VAA3D_COMPILE_FEDORA_DIR
-    cp -R VAA3D_COMPILE_REDHAT_DIR/v3a/* $VAA3D_COMPILE_FEDORA_DIR
+    echo "Creating symbolic link at $VAA3D_INSTALL_SYMLINK"
     rm $VAA3D_INSTALL_SYMLINK || true
     ln -s $VAA3D_INSTALL_REDHAT_DIR $VAA3D_INSTALL_SYMLINK
+
+    echo "Installing Vaa3D in $VAA3D_INSTALL_FEDORA_DIR"
+    rm -rf $VAA3D_INSTALL_FEDORA_DIR || true
+    cp -R $VAA3D_COMPILE_FEDORA_DIR/v3d $VAA3D_INSTALL_FEDORA_DIR
 fi
 
 ################################################################
@@ -60,11 +63,13 @@ fi
 ################################################################
 if [ $INSTALL_NEUSEP == 1 ]; then
     echo "Installing NeuronSeparator in $NEUSEP_INSTALL_REDHAT_DIR"
-    mkdir -p $NEUSEP_INSTALL_REDHAT_DIR
+    rm -rf $NEUSEP_INSTALL_REDHAT_DIR || true
     cd "$NEUSEP_COMPILE_REDHAT_DIR/build_cmake.redhat"
     cp mylib/sampsepNALoadRaw $NEUSEP_INSTALL_REDHAT_DIR
     cp tools/setup4 $NEUSEP_INSTALL_REDHAT_DIR
     cp tools/finish4 $NEUSEP_INSTALL_REDHAT_DIR
+
+    echo "Creating symbolic link at $NEUSEP_INSTALL_SYMLINK"
     rm $NEUSEP_INSTALL_SYMLINK || true
     ln -s $NEUSEP_INSTALL_REDHAT_DIR $NEUSEP_INSTALL_SYMLINK
 fi
@@ -72,27 +77,28 @@ fi
 ################################################################
 # Install Jacs to jacs and jacs-data servers
 ################################################################
-if [ $BUILD_JACS == 1 ]; then
+if [ $INSTALL_SERVER == 1 ]; then
     echo "Installing Jacs"
-    cd $JACS_COMPILE_DIR
-    cd compute
+    cd $JACS_COMPILE_DIR/compute
     echo "  Deploying to jacs"
     ant -Duser.server.machine=jacs "deploy-[your-server]-dev"
     echo "  Deploying to jacs-data"
     ant -Duser.server.machine=jacs-data "deploy-[your-server]-dev"
+    echo "FlySuite Version ${FWVER} (server) was successfully deployed to jacs and jacs-data."
 fi
 
 ################################################################
 # Install FlySuite Deployment Packages
 ################################################################
-if [ $BUILD_FLYSUITE == 1 ]; then
+if [ $INSTALL_CLIENT == 1 ]; then
     echo "Installing deployment packages"
+    rm -rf $FLYSUITE_INSTALL_DIR || true
+    rm -rf $FLYSUITE_LINUX_INSTALL_DIR || true
     cp -R $PACKAGE_MAC_DIR $FLYSUITE_INSTALL_DIR
     cp -R $PACKAGE_LINUX_DIR $FLYSUITE_LINUX_INSTALL_DIR
+    echo "FlySuite Version ${FWVER} (client) was successfully installed into the following locations:"
+    echo "  Mac: $FLYSUITE_INSTALL_DIR"
+    echo "  Linux: $FLYSUITE_LINUX_INSTALL_DIR"
+    echo ""
 fi
-
-echo "FlySuite Version ${FWVER} was successfully installed into the following locations:"
-echo "  Mac: $FLYSUITE_INSTALL_DIR"
-echo "  Linux: $FLYSUITE_LINUX_INSTALL_DIR"
-echo ""
 
