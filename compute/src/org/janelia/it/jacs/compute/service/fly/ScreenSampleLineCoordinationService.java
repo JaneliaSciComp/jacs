739cc163fd63dde40b41290244be8b5ec5ecbdec
Sean Murphy <murphys@janelia.hhmi.org>
2012-01-06 02:08:26 +0000

    Incremental progress on service.

--- compute/src/org/janelia/it/jacs/compute/service/fly/ScreenSampleLineCoordinationService.java
+++ compute/src/org/janelia/it/jacs/compute/service/fly/ScreenSampleLineCoordinationService.java
@@ -13,9 +13,12 @@ import org.janelia.it.jacs.compute.service.fileDiscovery.FlyScreenDiscoveryServi
 import org.janelia.it.jacs.model.entity.Entity;
 import org.janelia.it.jacs.model.entity.EntityConstants;
 import org.janelia.it.jacs.model.entity.EntityData;
+import org.janelia.it.jacs.model.entity.EntityType;
 import org.janelia.it.jacs.model.user_data.User;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by IntelliJ IDEA.
@@ -54,7 +57,7 @@ public class ScreenSampleLineCoordinationService implements IService {
             coordinateLinesAndSamples(flyLineList, screenSampleList);
             logger.info("After coordination step, flyLineList size="+flyLineList.size());
 
-            Long groupSize = processData.getLong("FLY_LINE_GROUP_SIZE");
+            Long groupSize = new Long(processData.getString("FLY_LINE_GROUP_SIZE").trim());
             logger.info("Using FLY_LINE_GROUP_SIZE="+groupSize);
 
             //List<List<Entity>> groupList = createFlyLineGroupList(flyLineList);
@@ -74,16 +77,7 @@ public class ScreenSampleLineCoordinationService implements IService {
     }
 
     protected List<Entity> getFlyLineList(Entity topLevelFolder) throws Exception {
-        List<Entity> flyLineList = new ArrayList<Entity>();
-        Set<EntityData> edList = topLevelFolder.getEntityData();
-        for (EntityData ed : edList) {
-            if (ed.getEntityAttribute().getName().equals(EntityConstants.TYPE_FLY_LINE)) {
-                Entity child = ed.getChildEntity();
-                if (child != null) {
-                    flyLineList.add(child);
-                }
-            }
-        }
+        List<Entity> flyLineList = getRecursiveEntitiesFromFolderByType(topLevelFolder, EntityConstants.TYPE_FLY_LINE);
         if (flyLineList.size() > 1) {
             Collections.sort(flyLineList, new Comparator<Entity>() {
                 @Override
@@ -96,16 +90,7 @@ public class ScreenSampleLineCoordinationService implements IService {
     }
 
     protected List<Entity> getScreenSampleList(Entity topLevelFolder) throws Exception {
-         List<Entity> screenSampleList = new ArrayList<Entity>();
-         Set<EntityData> edList = topLevelFolder.getEntityData();
-         for (EntityData ed : edList) {
-             if (ed.getEntityAttribute().getName().equals(EntityConstants.TYPE_SCREEN_SAMPLE)) {
-                 Entity child = ed.getChildEntity();
-                 if (child != null) {
-                     screenSampleList.add(child);
-                 }
-             }
-         }
+         List<Entity> screenSampleList = getRecursiveEntitiesFromFolderByType(topLevelFolder, EntityConstants.TYPE_SCREEN_SAMPLE);
          if (screenSampleList.size() > 1) {
              Collections.sort(screenSampleList, new Comparator<Entity>() {
                  @Override
@@ -117,6 +102,20 @@ public class ScreenSampleLineCoordinationService implements IService {
          return screenSampleList;
      }
 
+    protected List<Entity> getRecursiveEntitiesFromFolderByType(Entity folder, String typeName) throws Exception {
+        List<Entity> entityList = new ArrayList<Entity>();
+        Set<EntityData> edList = folder.getEntityData();
+        for (EntityData ed : edList) {
+            Entity child = ed.getChildEntity();
+            if (child != null && child.getEntityType().getName().equals(EntityConstants.TYPE_FOLDER)) {
+                entityList.addAll(getRecursiveEntitiesFromFolderByType(child, typeName));
+            } else if (child != null && child.getEntityType().getName().equals(typeName)) {
+                entityList.add(child);
+            }
+        }
+        return entityList;
+    }
+
     /*
 
         In this method, we want to make sure every sample has a FlyLine parent entity, and likewise that
@@ -124,7 +123,39 @@ public class ScreenSampleLineCoordinationService implements IService {
 
      */
     protected void coordinateLinesAndSamples(List<Entity> lineList, List<Entity> sampleList) throws Exception {
+        for (Entity sample : sampleList) {
+            String[] pwArr=getPlateAndWellPrefixesFromGMRName(sample.getName());
+            //logger.info("sampleName = "+sample.getName()+" platePrefix = "+pwArr[0]+" fullPrefix = "+pwArr[1]);
+        }
+    }
 
+    protected String[] getPlateAndWellPrefixesFromGMRName(String gmrName) throws Exception {
+        // Example GMR name: GMR_57B09_AE_01_02-mA01b_C070202_20070507160307953
+        try {
+            String[] u1Arr = gmrName.split("_");
+            if (u1Arr.length < 7) {
+                throw new Exception("u1 length=" + u1Arr.length);
+            }
+            String pw = u1Arr[1];
+            Pattern p = Pattern.compile("(\\d+)(\\D+)(\\d+)");
+            logger.info("Applying matcher to string=" + pw);
+            Matcher m = p.matcher(pw);
+            if (m.lookingAt()) {
+                if (m.groupCount() < 3) {
+                    throw new Exception("pw group count=" + m.groupCount());
+                }
+                String plate = m.group(1);
+                String well = m.group(2) + "" + m.group(3);
+                String[] result = new String[2];
+                result[0] = u1Arr[0] + "_" + plate;       // GMR_57
+                result[1] = u1Arr[0] + "_" + u1Arr[1];    // GMR_57B09
+                return result;
+            } else {
+                throw new Exception("lookingAt returned false - could not match " + pw);
+            }
+        } catch (Exception ex) {
+            throw new Exception(ex.getMessage() + " : could not parse GMR name=" + gmrName);
+        }
     }
 
 
