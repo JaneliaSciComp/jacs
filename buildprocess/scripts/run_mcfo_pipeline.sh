9318e35185eb3505b0a9fa8448bffc8eff56c409
Konrad Rokicki <rokickik@janelia.hhmi.org>
2012-01-09 16:37:50 +0000

    fixed to work with new pipeline

--- buildprocess/scripts/run_mcfo_pipeline.sh
+++ buildprocess/scripts/run_mcfo_pipeline.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-/usr/bin/wget -v --post-data="action=invokeOpByName&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodName=runMCFODataPipeline&argType=java.lang.String&arg0=system&argType=boolean&arg1=False&argType=java.lang.String&arg2=/groups/flylight/flylight/flip/SecData/tiles&argType=java.lang.String&arg3=FlyLight+Single+Neuron+Data" http://$1:8180/jmx-console/HtmlAdaptor
-
+/usr/bin/wget -v --post-data="action=invokeOpByName&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodName=runMCFODataPipeline&argType=java.lang.String&arg0=system&argType=java.lang.String&arg1=/groups/flylight/flylight/flip/SecData/tiles&argType=java.lang.String&arg2=FlyLight+Single+Neuron+Data&argType=boolean&arg3=False&argType=boolean&arg4=False&argType=boolean&arg5=False" http://$1:8180/jmx-console/HtmlAdaptor
