37bad9c855bdf7fc22cf31d4e0223d2861369e86
Konrad Rokicki <rokickik@janelia.hhmi.org>
2012-01-17 14:39:30 +0000

    fixed script

--- buildprocess/scripts/run_mcfo_pipeline.sh
+++ buildprocess/scripts/run_mcfo_pipeline.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/usr/bin/wget -v --post-data="action=invokeOpByName&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodName=runMCFODataPipeline&argType=java.lang.String&arg0=system&argType=java.lang.String&arg1=/groups/flylight/flylight/flip/SecData/tiles&argType=java.lang.String&arg2=FlyLight+Single+Neuron+Data&argType=boolean&arg3=False&argType=boolean&arg4=False&argType=boolean&arg5=False" http://$1:8180/jmx-console/HtmlAdaptor
+/usr/bin/wget -v --post-data="action=invokeOpByName&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodName=runMCFODataPipeline&argType=java.lang.String&arg0=system&argType=java.lang.String&arg1=/groups/flylight/flylight/flip/SecData/tiles&argType=java.lang.String&arg2=FlyLight+Single+Neuron+Data&argType=boolean&arg3=False&argType=boolean&arg4=False&argType=boolean&arg5=False" http://${1}:8180/jmx-console/HtmlAdaptor
+
