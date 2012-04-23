0319c058519662052d1c1fef4f54b93dba9d9656
Konrad Rokicki <rokickik@janelia.hhmi.org>
2012-04-23 22:07:07 +0000

    fixed argument index

--- buildprocess/scripts/run_mcfo_pipeline.sh
+++ buildprocess/scripts/run_mcfo_pipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/bin/wget -v --post-data="action=invokeOpByName&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodName=runMCFODataPipeline&argType=java.lang.String&arg0=system&argType=java.lang.String&arg2=FlyLight+Single+Neuron+Data&argType=java.lang.Boolean&arg3=False&argType=java.lang.Boolean&arg4=False&argType=java.lang.Boolean&arg5=False" http://${1}:8180/jmx-console/HtmlAdaptor
+/usr/bin/wget -v --post-data="action=invokeOpByName&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodName=runMCFODataPipeline&argType=java.lang.String&arg0=system&argType=java.lang.String&arg1=FlyLight+Single+Neuron+Data&argType=java.lang.Boolean&arg2=False&argType=java.lang.Boolean&arg3=False&argType=java.lang.Boolean&arg4=False" http://${1}:8180/jmx-console/HtmlAdaptor
 
