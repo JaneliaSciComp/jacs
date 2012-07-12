e08177a1209dd24b4122b3fc7f3f56f13593bee7
Konrad Rokicki <rokickik@janelia.hhmi.org>
2012-07-12 16:18:32 +0000

    refactored search configuration so that it can be shared with other dialogs added SAGE properties to entity details dialog

--- shared/src/org/janelia/it/jacs/shared/utils/StringUtils.java
+++ shared/src/org/janelia/it/jacs/shared/utils/StringUtils.java
@@ -1,5 +1,7 @@
 package org.janelia.it.jacs.shared.utils;
 
+import java.util.Collection;
+
 /**
  * Some helpful utilities for strings.
  * 
@@ -10,5 +12,30 @@ public class StringUtils {
 	public static boolean isEmpty(String s) {
 		return s==null || "".equals(s);
 	}
-	
+
+	public static String underscoreToTitleCase(String name) {
+    	String[] words = name.split("_");
+    	StringBuffer buf = new StringBuffer();
+    	for(String word : words) {
+    		char c = Character.toUpperCase(word.charAt(0));
+    		if (buf.length()>0) buf.append(' ');
+    		buf.append(c);
+    		buf.append(word.substring(1).toLowerCase());
+    	}
+    	return buf.toString();
+    }
+
+	public static String getCommaDelimited(Collection objs, int maxLength) {
+		if (objs==null) return null;
+		StringBuffer buf = new StringBuffer();
+		for(Object obj : objs) {
+			if (buf.length()+3>=maxLength) {
+				buf.append("...");
+				break;
+			}
+			if (buf.length()>0) buf.append(", ");
+			buf.append(obj.toString());
+		}
+		return buf.toString();
+	}
 }
