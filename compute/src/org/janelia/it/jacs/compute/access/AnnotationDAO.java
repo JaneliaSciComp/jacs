37d77c359a1584a918323d419e17a6dd25817046
Konrad Rokicki <rokickik@janelia.hhmi.org>
2012-07-16 22:26:53 +0000

    clean up

--- compute/src/org/janelia/it/jacs/compute/access/AnnotationDAO.java
+++ compute/src/org/janelia/it/jacs/compute/access/AnnotationDAO.java
@@ -17,7 +17,6 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.criterion.Expression;
 import org.janelia.it.jacs.compute.api.ComputeException;
-import org.janelia.it.jacs.compute.api.support.EntityMapStep;
 import org.janelia.it.jacs.compute.api.support.MappedId;
 import org.janelia.it.jacs.compute.service.fly.MaskSampleAnnotationService;
 import org.janelia.it.jacs.model.common.SystemConfigurationProperties;
