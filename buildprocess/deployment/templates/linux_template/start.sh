de39a397d5356a8633958f18ccc35a161a559720
Konrad Rokicki <rokickik@janelia.hhmi.org>
2012-08-31 15:35:30 +0000

    removed NA from startup script, users can manually start it via the tools menu

--- buildprocess/deployment/templates/linux_template/start.sh
+++ buildprocess/deployment/templates/linux_template/start.sh
@@ -2,5 +2,5 @@
 DIR=$(cd "$(dirname "$0")"; pwd)
 cd $DIR
 java -Dsun.awt.disableMixing=true -XX:+UseParallelGC -XX:-UseGCOverheadLimit -jar workstation.jar -Xms512m -Xmx2560m &
-sleep 3
-./vaa3d -na &
+#sleep 3
+#./vaa3d -na &
--- buildprocess/deployment/templates/mac_template/start.sh
+++ buildprocess/deployment/templates/mac_template/start.sh
@@ -2,6 +2,6 @@
 DIR=$(cd "$(dirname "$0")"; pwd)
 cd $DIR
 java -Dsun.awt.disableMixing=true -XX:+UseParallelGC -XX:-UseGCOverheadLimit -jar workstation.jar -Xms512m -Xmx2560m > console.log 2>&1 &
-sleep 3
-./vaa3d64.app/Contents/MacOS/vaa3d64 -na > vaa3d.log 2>&1 &
+#sleep 3
+#./vaa3d64.app/Contents/MacOS/vaa3d64 -na > vaa3d.log 2>&1 &
 
