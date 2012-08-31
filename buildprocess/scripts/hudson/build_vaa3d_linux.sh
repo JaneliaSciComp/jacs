2511c51ae237678bce04efa75d7b13e545f89207
Konrad Rokicki <rokickik@janelia.hhmi.org>
2012-08-31 14:34:52 +0000

    switch to CMake build for fastload capability

--- buildprocess/scripts/hudson/build_vaa3d_linux.sh
+++ buildprocess/scripts/hudson/build_vaa3d_linux.sh
@@ -4,39 +4,27 @@ export JACS_INSTALL_DIR="/groups/scicomp/jacsData/install"
 
 if [ -f /etc/fedora-release ] ; then
     export QTDIR="${JACS_INSTALL_DIR}/Qt-4.7.4-fedora"
-    export INCLUDEPATH="${JACS_INSTALL_DIR}/tiff-3.9.5-fedora/include"
+    export CMAKE_DIR="${JACS_INSTALL_DIR}/cmake-2.8.9-fedora"
+    export BOOST_DIR="${JACS_INSTALL_DIR}/boost-1.51.0-fedora"
 else
     export QTDIR="${JACS_INSTALL_DIR}/Qt-4.7.4-redhat"
-    export INCLUDEPATH="${JACS_INSTALL_DIR}/tiff-3.9.5-redhat/include"
+    export CMAKE_DIR="${JACS_INSTALL_DIR}/cmake-2.8.9-redhat"
+    export BOOST_DIR="${JACS_INSTALL_DIR}/boost-1.51.0-redhat"
 fi
 
 export QTLIB=$QTDIR/lib
 export QTINC=$QTDIR/include
-
 export PATH=$QTDIR/bin:$PATH
 export PATH=$HOME/bin:$PATH
-
-./build.linux -B -m -j4
 export BASE=`pwd`
+export BUILD_DIR=$BASE/build_cmake
+export BIN_DIR=$BASE/bin
 
-# build brainaligner
-cd $BASE/jba/c++
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+${CMAKE_DIR}/bin/cmake -DUSE_FFMPEG=ON -DBoost_INCLUDE_DIR=${BOOST_DIR} ..
 make
-cp $BASE/jba/c++/brainaligner $BASE/bin/
-
-# build lobeseg
-cd $BASE/released_plugins/v3d_plugins/lobeseg/lobeseg_main
-cp main_backgnd_lobeseg.makefile Makefile
-make
-cp $BASE/released_plugins/v3d_plugins/lobeseg/lobeseg_main/lobe_seger $BASE/bin/
-
-# build optic lobe aligner
-#cd $BASE/released_plugins/v3d_plugins/ssd_registration
-#echo "INCLUDEPATH += $INCLUDEPATH" >> plugin_PQ_imagereg.pro
-#qmake
-#make
-#cp -R $BASE/released_plugins/v3d/plugins/optic_lobe_aligner $BASE/v3d/plugins
-
-# copy libs
-cp -R $BASE/v3d_main/common_lib/lib $BASE/bin/
+cp -R $BUILD_DIR/v3d/Linux_64/* $BIN_DIR
+cp $BUILD_DIR/lobe_seger $BIN_DIR
+cp $BUILD_DIR/brainaligner $BIN_DIR
 
--- buildprocess/scripts/hudson/build_vaa3d_mac.sh
+++ buildprocess/scripts/hudson/build_vaa3d_mac.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+export JACS_INSTALL_DIR="/groups/scicomp/jacsData/install"
 export QTDIR="/groups/scicomp/jacsData/install/Qt-4.7.4-mac"
+export CMAKE_DIR="${JACS_INSTALL_DIR}/cmake-2.8.9-mac/CMake-2.8-9.app/Contents"
+export BOOST_DIR="${JACS_INSTALL_DIR}/boost-1.51.0-mac"
 export QTLIB=$QTDIR/lib
 export QTINC=$QTDIR/include
-
 export PATH=$QTDIR/bin:$PATH
+export BASE=`pwd`
+export BUILD_DIR=$BASE/build_cmake
+export BIN_DIR=$BASE/bin
 
-./build.macx -B -m -j4 -norun
-
-$QTDIR/bin/macdeployqt v3d_main/v3d/vaa3d64.app
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+${CMAKE_DIR}/bin/cmake -DUSE_FFMPEG=ON -DBoost_INCLUDE_DIR=${BOOST_DIR} ..
+make
+cp -R $BUILD_DIR/v3d/Mac_Fat/* $BIN_DIR
 
