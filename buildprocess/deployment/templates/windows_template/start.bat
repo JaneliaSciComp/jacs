6ba083b096b55e07c007d2e6e67064e7041c53a3
Les Foster <fosterl@janelia.hhmi.org>
2012-12-10 19:33:20 +0000

    Preparing the templates batch files.  These allow the user to run a standard command that updates and launches the Fly Suite seamlessly.

--- buildprocess/deployment/templates/windows_template/start.bat
+++ buildprocess/deployment/templates/windows_template/start.bat
@@ -1,6 +1,9 @@
-#!/bin/sh
-DIR=$(cd "$(dirname "$0")"; pwd)
-cd $DIR
+@ECHO OFF
+:: Go to the directory of the script itself, before launching commands, to ensure everything is where needed.
+set DIR=%~dp0
+cd %DIR%
 java -XX:+UseParallelGC -jar workstation.jar -Xms512m -Xmx1024m &
-sleep 3
-./vaa3d -na &
+:: Best yet approach to a sleep 3 command equivalent.
+choice /N /C y /D y /T 3 > NUL
+:: Run Vaa 3D in background
+START /B .\vaa3d -na
--- buildprocess/deployment/templates/windows_template/workstation.bat
+++ buildprocess/deployment/templates/windows_template/workstation.bat
@@ -1,36 +1,41 @@
-#!/bin/sh
-#
-# Check for updates before starting the FlySuite.
-#
+@ECHO OFF
+::
+:: Check for updates before starting the FlySuite.  NOTE: this script should never run on Mac, so no
+:: extra-pathing info will be checked.
+::
 
-TEMP=/tmp
-INSTALL=$(cd "$(dirname "$0")"; pwd)
-EXTRA=""
-if echo "$INSTALL" |grep -q "/Contents/Resources"; then
-    # on Mac, get rid of the extra path from the app bundle root
-    INSTALL=${INSTALL%Contents/Resources}
-    EXTRA="/Contents/Resources"
-fi
+set INSTALL=%~dp0
 
-DOWNLOAD=`java -cp workstation.jar -Xms512m -Xmx1024m org.janelia.it.FlyWorkstation.gui.application.AutoUpdater | tail -1`
-if [ "$DOWNLOAD" != "" ] && [ -e $DOWNLOAD ] ; then
-  echo "Updater downloaded a new version to $DOWNLOAD."
+:: Reading the last line of the file produced by running the workstation AutoUpdater application.
+::  The last line should be the full path to a downloaded update.
+::
+java -cp workstation.jar -Xms512m -Xmx1024m org.janelia.it.FlyWorkstation.gui.application.AutoUpdater > autoupdate.log
+set DOWNLOAD=
+FOR /F "usebackq delims=" %%i IN (autoupdate.log) DO set DOWNLOAD=%%i
+if NOT "%DOWNLOAD%"=="" (
+    echo Updater downloaded a new version to %DOWNLOAD%.
 
-  cat > $TEMP/updateScript.sh << EOF
-#!/bin/sh
-if cp -fR $DOWNLOAD/* $INSTALL; then
-  echo "done. Update complete."
-else
-  echo "failed!"
-fi
-sh ${INSTALL}${EXTRA}/start.sh
-EOF
+    ::  Now need to create a temporary batch file to run the update, which replaces the running batch script as well.
+    ::
+    set UPDATEBAT=%TEMP%\update__workstation.bat
+    echo @echo off                            > %UPDATEBAT%
+    echo Updating...                         >> %UPDATEBAT%
+    echo rmdir /S /Q %INSTALL%               >> %UPDATEBAT%
+    echo xcopy /S %DOWNLOAD% %INSTALL%\      >> %UPDATEBAT%
+    echo IF NOT EXISTS %INSTALL%\workstation.bat ( >> %UPDATEBAT%
+    echo   echo failed!                      >> %UPDATEBAT%
+    echo   exit 0                            >> %UPDATEBAT%
+    echo )                                   >> %UPDATEBAT%
+    echo echo done.  Update complete.        >> %UPDATEBAT%
+    echo COMMAND /S %INSTALL%\start.bat      >> %UPDATEBAT%
 
-  echo -n "Executing update..."
-  exec sh $TEMP/updateScript.sh  
-  exit 1
-fi
-
-echo "Already at latest version."
-sh start.sh
-
+    :: Now run the file created above.  Exit afterwards.
+    ::
+    echo Executing update...
+    call %UPDATEBAT%
+    exit 1
+) ELSE (
+    echo Already at latest version.
+    call %INSTALL%\start.bat
+    exit 1
+)
