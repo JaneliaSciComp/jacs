7a266826286da4b8e53b8e85e090e4ab9ffc2ae1
Les Foster <fosterl@janelia.hhmi.org>
2012-12-11 20:22:27 +0000

    Working workstation.bat and start.bat

--- buildprocess/deployment/templates/windows_template/start.bat
+++ buildprocess/deployment/templates/windows_template/start.bat
@@ -2,8 +2,9 @@
 :: Go to the directory of the script itself, before launching commands, to ensure everything is where needed.
 set DIR=%~dp0
 cd %DIR%
-java -XX:+UseParallelGC -jar workstation.jar -Xms512m -Xmx1024m &
+START /B java -XX:+UseParallelGC -jar workstation.jar -Xms512m -Xmx1024m
+::  No longer need to launch vaa3d from the batch script.
 :: Best yet approach to a sleep 3 command equivalent.
-choice /N /C y /D y /T 3 > NUL
+::choice /N /C y /D y /T 3 > NUL
 :: Run Vaa 3D in background
-START /B .\vaa3d -na
+::START /B .\vaa3d -na
--- buildprocess/deployment/templates/windows_template/workstation.bat
+++ buildprocess/deployment/templates/windows_template/workstation.bat
@@ -11,22 +11,30 @@ cd %INSTALL%
 ::  The last line should be the full path to a downloaded update.
 ::
 java -cp workstation.jar -Xms512m -Xmx1024m org.janelia.it.FlyWorkstation.gui.application.AutoUpdater > autoupdate.log
+
+::  Get last line of file.  If file contains a path to new installation, it will be that one.
+::
 set DOWNLOAD=
-FOR /F "usebackq delims=" %%i IN (autoupdate.log) DO set DOWNLOAD=%%i
-if NOT "%DOWNLOAD%"=="" (
+FOR /F "delims=" %%i IN (autoupdate.log) DO set DOWNLOAD=%%i
+
+echo The putative download line is %DOWNLOAD%
+::  Search the auto updater log for the string indicating no update needed.
+::
+FINDSTR /m "Already at latest version" autoupdate.log >NUL
+IF ErrorLevel 0 goto Latest
     echo Updater downloaded a new version to %DOWNLOAD%.
 
     ::  Now need to create a temporary batch file to run the update, which replaces the running batch script as well.
     ::
-    set UPDATEBAT=%TEMP%\update__workstation.bat
+    set UPDATEBAT="%TEMP%"\update__workstation.bat
+	del "%UPDATEBAT%"
     echo @echo off                            > %UPDATEBAT%
-    echo Updating...                         >> %UPDATEBAT%
-    echo rmdir /S /Q %INSTALL%               >> %UPDATEBAT%
+    echo echo Updating...                    >> %UPDATEBAT%
+    echo echo rmdir /S /Q %INSTALL%               >> %UPDATEBAT%
     echo xcopy /S %DOWNLOAD% %INSTALL%\      >> %UPDATEBAT%
-    echo IF NOT EXISTS %INSTALL%\workstation.bat ( >> %UPDATEBAT%
-    echo   echo failed!                      >> %UPDATEBAT%
-    echo   exit 0                            >> %UPDATEBAT%
-    echo )                                   >> %UPDATEBAT%
+	::  Parens echoed here are confused with ending of IF statement enclosing this block of code.
+    echo IF NOT EXIST %INSTALL%\workstation.bat echo failed! >> %UPDATEBAT%
+    echo IF NOT EXIST %INSTALL%\workstation.bat exit 0 >> %UPDATEBAT%
     echo echo done.  Update complete.        >> %UPDATEBAT%
     echo COMMAND /S %INSTALL%\start.bat      >> %UPDATEBAT%
 
@@ -34,9 +42,14 @@ if NOT "%DOWNLOAD%"=="" (
     ::
     echo Executing update...
     call %UPDATEBAT%
-    exit 1
-) ELSE (
+
+	goto Finish
+	
+:Latest
     echo Already at latest version.
-    call %INSTALL%\start.bat
-    exit 1
-)
+	cd %INSTALL%
+    call start.bat
+	goto Finish
+
+:Finish
+	::exit 1
