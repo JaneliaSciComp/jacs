20f775c07517f704a3d58de67a18b69b77341cbe
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-01-04 21:32:22 +0000

    support for v3dpbd

--- compute/scripts/single_neuron/BrainAligner/run_configured_aligner.sh
+++ compute/scripts/single_neuron/BrainAligner/run_configured_aligner.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DIR=$(cd "$(dirname "$0")"; pwd)
+Vaa3D="$DIR/../Toolkits/Vaa3D/vaa3d"
+
 SCRIPT_PATH=$1
 CONFIG_PATH=$2
 TEMPLATE_DIR=$3
@@ -19,6 +22,15 @@ echo "~ Alignment Config: $CONFIG_PATH"
 echo "~ Template Dir: $TEMPLATE_DIR"
 echo "~ Run Dir: $DIR"
 echo "~ Working Dir: $WORKING_DIR"
+
+EXT=${INPUT_FILE#*.}
+if [ $EXT == "v3dpbd" ]; then
+    PBD_INPUT_FILE=$INPUT_FILE
+    INPUT_FILE_STUB=`basename $PBD_INPUT_FILE`
+    INPUT_FILE="$WORKING_DIR/${INPUT_FILE_STUB%.*}.v3draw"
+    echo "~ Converting $PBD_INPUT_FILE to $INPUT_FILE "
+    $Vaa3D -cmd image-loader -convert "$PBD_INPUT_FILE" "$INPUT_FILE"
+fi
 
 CMD="$SCRIPT_PATH $CONFIG_PATH $TEMPLATE_DIR $TOOLKITS_DIR $WORKING_DIR $INPUT_FILE $REF_CHANNEL $OPTICAL_RESOLUTION"
 echo $CMD
