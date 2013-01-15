f17bf45613f4dea362c5dbeb5be2272eae703230
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-01-15 19:23:54 +0000

    added group members

--- compute/src/org/janelia/it/jacs/compute/service/entity/UpgradeUserDataService.java
+++ compute/src/org/janelia/it/jacs/compute/service/entity/UpgradeUserDataService.java
@@ -1,5 +1,6 @@
 package org.janelia.it.jacs.compute.service.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -7,7 +8,6 @@ import org.janelia.it.jacs.compute.service.fly.ScreenScoresLoadingService;
 import org.janelia.it.jacs.model.entity.Entity;
 import org.janelia.it.jacs.model.entity.EntityConstants;
 import org.janelia.it.jacs.model.user_data.Group;
-import org.janelia.it.jacs.model.user_data.SubjectRelationship;
 import org.janelia.it.jacs.model.user_data.User;
 import org.janelia.it.jacs.shared.utils.EntityUtils;
 
@@ -20,86 +20,98 @@ public class UpgradeUserDataService extends AbstractEntityService {
 
 	private static final Logger logger = Logger.getLogger(UpgradeUserDataService.class);
 
-	private static final String[] admins = { "saffordt", "rokickik", "brunsc", "fosterl", "murphys", "trautmane", "yuy" };
+	private static final String[] adminMembers = { "saffordt", "rokickik", "brunsc", "fosterl", "murphys", "trautmane", "yuy" };
 	
+	private static final String[] leetlabMembers = { "saffordt", "rokickik", "yuy", "fosterl", "chenh12", "huangy11", "jingx", "leetz", "leey10", "liuz10", "luanh", "renq", "schroederm", "wangy14" };
+	
+    private static final String[] cardlabMembers = { "saffordt", "wum10" };
+    
+    private static final String[] simpsonlabMembers = { "saffordt", "midgleyf", "weaverc10" };
+    
     public void execute() throws Exception {
         
         final String serverVersion = computeBean.getAppVersion();
         logger.info("Updating data model to latest version: "+serverVersion);
 
+        List<String> allUsers = new ArrayList<String>();
+        for(User user : computeBean.getUsers()) {   
+            if (!user.getName().equals(User.SYSTEM_USER_LOGIN)) {
+                allUsers.add(user.getName());
+            }
+        }
+ 
         // ------------------------------------------------------------------------------------------------------------
-        // Create System user
+        // Create users and groups
         // ------------------------------------------------------------------------------------------------------------
-        
+
         computeBean.createUser(User.SYSTEM_USER_LOGIN, "System");
+        computeBean.createUser("chenh12", "Hui-Min Chen");
+        computeBean.createUser("huangy11", "Yu-Fen Huang");
+        computeBean.createUser("jingx", "Xiaotang Jing");
+        computeBean.createUser("liuz10", "Zhiyong Liu");
+        computeBean.createUser("luanh", "Haojiang Luan");
+        
+        Group flylightGroup = getOrCreateGroup("flylight", "FlyLight", User.SYSTEM_USER_KEY);
+        Group adminGroup = getOrCreateGroup(Group.ADMIN_GROUP_NAME, "Administrators", User.SYSTEM_USER_KEY);
+        
         
         // ------------------------------------------------------------------------------------------------------------
-        // Create FlyLight group
+        //  Add Group Members
         // ------------------------------------------------------------------------------------------------------------
         
-        Group flylightGroup = computeBean.getGroupByNameOrKey("group:flylight");
-        flylightGroup.getUserRelationships().clear();
-        computeBean.saveOrUpdateGroup(flylightGroup);
-
-        List<User> allUsers = computeBean.getUsers();
-        for(User user : allUsers) {	
-        	SubjectRelationship sr = new SubjectRelationship(user, flylightGroup, 
-        			user.getName().equals(User.SYSTEM_USER_LOGIN) ? 
-        					SubjectRelationship.TYPE_GROUP_OWNER : 
-        					SubjectRelationship.TYPE_GROUP_MEMBER);
-            logger.info("Adding "+user.getName()+" to group "+flylightGroup.getName()+" as "+sr.getRelationshipType());
-        	flylightGroup.getUserRelationships().add(sr);
-        }
-        computeBean.saveOrUpdateGroup(flylightGroup);
-
-        // ------------------------------------------------------------------------------------------------------------
-        //  Create Administrator Group
-        // ------------------------------------------------------------------------------------------------------------
-        
-        Group adminGroup = computeBean.getGroupByNameOrKey(Group.ADMIN_GROUP_KEY);
-        
-        if (adminGroup!=null) {
-            adminGroup.getUserRelationships().clear();
-            adminGroup.setFullName("Administrators");
-            computeBean.saveOrUpdateGroup(adminGroup);
-        }
-        else {
-        	adminGroup = computeBean.createGroup(User.SYSTEM_USER_KEY, Group.ADMIN_GROUP_NAME);
-        	adminGroup.setFullName("Administrators");
-        }
-        
-        for(String username : admins) {
-            User admin = computeBean.getUserByNameOrKey(username);
-            if (admin==null) {
-            	logger.error("Could not find user: "+username);
-            }
-            else {
-	        	SubjectRelationship sr = new SubjectRelationship(admin, adminGroup, SubjectRelationship.TYPE_GROUP_MEMBER);
-	            logger.info("Adding "+username+" to group "+adminGroup.getName()+" as "+sr.getRelationshipType());
-	            adminGroup.getUserRelationships().add(sr);
-            }
-        }
-
-        computeBean.saveOrUpdateGroup(adminGroup);
+        addGroupMembers(flylightGroup, allUsers.toArray(new String[0]));
+        addGroupMembers(adminGroup, adminMembers);
+        addGroupMembers("leetlab", leetlabMembers);
+        addGroupMembers("cardlab", cardlabMembers);
+        addGroupMembers("simpsonlab", simpsonlabMembers);
         
         // ------------------------------------------------------------------------------------------------------------
         // Add Protected tags
         // ------------------------------------------------------------------------------------------------------------
         
-        for(User user : allUsers) {	
-        	for(Entity commonRoot : annotationBean.getCommonRootEntities(user.getKey())) {
+        for(String username : allUsers) {	
+            String subjectKey = "user:"+username;
+            logger.info("Processing common roots for "+username); 
+        	for(Entity commonRoot : annotationBean.getCommonRootEntities(subjectKey)) {
+        	    if (!EntityUtils.isOwner(commonRoot, subjectKey)) continue;
         		if (EntityConstants.NAME_MY_DATA_SETS.endsWith(commonRoot.getName()) 
         				|| EntityConstants.NAME_PUBLIC_DATA_SETS.endsWith(commonRoot.getName()) 
         				|| EntityConstants.NAME_SPLIT_PICKING.endsWith(commonRoot.getName())
         				|| ScreenScoresLoadingService.TOP_LEVEL_EVALUATION_FOLDER.endsWith(commonRoot.getName())
         				|| "FlyLight Screen Split Lines".endsWith(commonRoot.getName())
         				|| "Arnim Data Combinations".endsWith(commonRoot.getName())) {
-        			EntityUtils.addAttributeAsTag(commonRoot, EntityConstants.ATTRIBUTE_IS_PROTECTED);
+        		    logger.info("  Ensuring protection for "+commonRoot.getName()+" (id="+commonRoot.getId()+")"); 
+        		    EntityUtils.addAttributeAsTag(commonRoot, EntityConstants.ATTRIBUTE_IS_PROTECTED);
             		entityBean.saveOrUpdateEntity(commonRoot);
         		}
         	}
         }
-        
-        
+    }
+    
+    private Group getOrCreateGroup(String groupName, String groupFullName, String ownerKey) throws Exception {
+        Group group = computeBean.getGroupByNameOrKey(groupName);
+        if (group==null) {
+            group = computeBean.createGroup(ownerKey, groupName);
+        }
+        group.getUserRelationships().clear();
+        group.setFullName(groupFullName);
+        computeBean.saveOrUpdateGroup(group);
+        return group;
+    }
+    
+    private void addGroupMembers(String groupName, String[] members) throws Exception {
+        Group group = computeBean.getGroupByNameOrKey(groupName);
+        if (group==null) {
+            throw new IllegalStateException("Group does not exist: "+groupName);
+        }
+        group.getUserRelationships().clear();
+        computeBean.saveOrUpdateGroup(group);
+        addGroupMembers(group, members);
+    }
+    
+    private void addGroupMembers(Group group, String[] members) throws Exception {
+        for(String username : members) {
+            computeBean.addUserToGroup(username, group.getName());
+        }
     }
 }
