a325d0f69e52dc84920cfb439602bb485a07935d
Todd Safford <saffordt@janelia.hhmi.org>
2013-01-25 15:21:14 +0000

    From Branch: Fix for login and user changes

--- jacs/src/org/janelia/it/jacs/web/control/UserVerificationInterceptor.java
+++ jacs/src/org/janelia/it/jacs/web/control/UserVerificationInterceptor.java
@@ -1,19 +1,17 @@
 
 package org.janelia.it.jacs.web.control;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.log4j.Logger;
 import org.janelia.it.jacs.compute.api.ComputeBeanRemote;
-import org.janelia.it.jacs.model.user_data.Subject;
 import org.janelia.it.jacs.model.user_data.User;
 import org.janelia.it.jacs.server.access.UserDAO;
 import org.janelia.it.jacs.web.security.JacsSecurityUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -87,22 +85,12 @@ public class UserVerificationInterceptor extends HandlerInterceptorAdapter {
     }
 
     protected User obtainUser(String userLogin) throws Exception {
-        try {
-            User user = userDAO.getUserByName(userLogin);
-            if (null == user) {
-                Subject subject = computeServerBean.login(userLogin, null);
-                if (subject==null) {
-                    // will not be able to execute any computes, so throw an exception
-                    throw new IOException("Unable to login user " + userLogin);
-                }
-                user = userDAO.getUserByName(userLogin);
-            }
-            return user;
+        User user = userDAO.getUserByName(userLogin);
+        if (null == user) {
+            // will not be able to execute any computes, so throw an exception
+            throw new IOException("Unable to login user " + userLogin);
         }
-        catch (Exception e) {
-            logger.error("Exception: " + e.getMessage(), e);
-            throw e;
-        }
+        return user;
     }
 
 }
