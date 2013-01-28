3973a95fb4668e9a6051b2f3ba21d8f600647086
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-01-28 16:02:24 +0000

    added argument

--- buildprocess/scripts/run_data_set_pipeline.sh
+++ buildprocess/scripts/run_data_set_pipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/usr/bin/wget -v --post-data="action=invokeOpByName&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodName=runAllDataSetPipelines&argType=java.lang.String&arg0=NEW" http://${1}:8180/jmx-console/HtmlAdaptor
+/usr/bin/wget -v --post-data="action=invokeOpByName&name=ComputeServer%3Aservice%3DWorkstationDataManager&methodName=runAllDataSetPipelines&argType=java.lang.String&arg0=NEW&argType=java.lang.Boolean&arg1=False" http://${1}:8180/jmx-console/HtmlAdaptor
 
