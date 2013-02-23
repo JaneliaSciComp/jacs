8c4307b24dad8acb2c0d8cdd3ba649736338a2bf
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-02-23 18:23:32 +0000

    exports for lee lab

--- groovy/src/SubmitSamples.groovy
+++ groovy/src/SubmitSamples.groovy
@@ -38,47 +38,52 @@ for (Entity sample : sampleFolder.children) {
 	f.loadChildren(sample)
 	sc = EntityUtils.getOrderedEntityDataForAttribute(sample,ATTRIBUTE_ENTITY)
 	
-    int numDefaultImages = 0;
-    for(EntityData pipelineRunEd : sc) {
-        if (pipelineRunEd.childEntity.getValueByAttributeName(ATTRIBUTE_DEFAULT_2D_IMAGE)!=null) {
-            numDefaultImages++;
-        }
-    }
+    pipelineRun = EntityUtils.getLatestChildOfType(sample, TYPE_PIPELINE_RUN);
     
-    boolean reprocess = false;
-    if (sc.size()>=2) {
-        // skip
-        if (numDefaultImages<2) {
-            reprocess = true;
-        }
-    }
-    else if (sc.size()==1) {
-        reprocess = true;
-    }
-    else {
-        println "UNKNOWN sample: "+sample.name
-        for(EntityData child : sc) {
-            println "    "+child.childEntity.name
-        }
-        reprocess = true;
-    }	
+    if (pipelineRun.creationDate==null || pipelineRun.creationDate.toString().startsWith("2012"))
+        println sample.name+"\t"+pipelineRun.creationDate
     
-    if (reprocess) {
-        println "Reprocess "+sample.name+" for "+sample.ownerKey
-        HashSet<TaskParameter> taskParameters = new HashSet<TaskParameter>();
-        taskParameters.add(new TaskParameter("sample entity id", sample.id.toString(), null));
-        taskParameters.add(new TaskParameter("reuse processing", "false", null));
-        Task task = new GenericTask(new HashSet<Node>(), "leetlab", new ArrayList<Event>(),
-                taskParameters, "flylightSampleAllPipelines", "Flylight Sample All Pipelines");
-        task.setJobName("Flylight Sample All Pipelines Task");
-        task = c.saveOrUpdateTask(task);
-        c.submitJob("GSPS_CompleteSamplePipeline", task.getObjectId());
-        numSamplesReprocessed++;
-    }
+//    int numDefaultImages = 0;
+//    for(EntityData pipelineRunEd : sc) {
+//        if (pipelineRunEd.childEntity.getValueByAttributeName(ATTRIBUTE_DEFAULT_2D_IMAGE)!=null) {
+//            numDefaultImages++;
+//        }
+//    }
+//    
+//    boolean reprocess = false;
+//    if (sc.size()>=2) {
+//        // skip
+//        if (numDefaultImages<2) {
+//            reprocess = true;
+//        }
+//    }
+//    else if (sc.size()==1) {
+//        reprocess = true;
+//    }
+//    else {
+//        println "UNKNOWN sample: "+sample.name
+//        for(EntityData child : sc) {
+//            println "    "+child.childEntity.name
+//        }
+//        reprocess = true;
+//    }	
+//    
+//    if (reprocess) {
+//        println "Reprocess "+sample.name+" for "+sample.ownerKey
+//        HashSet<TaskParameter> taskParameters = new HashSet<TaskParameter>();
+//        taskParameters.add(new TaskParameter("sample entity id", sample.id.toString(), null));
+//        taskParameters.add(new TaskParameter("reuse processing", "false", null));
+//        Task task = new GenericTask(new HashSet<Node>(), "leetlab", new ArrayList<Event>(),
+//                taskParameters, "flylightSampleAllPipelines", "Flylight Sample All Pipelines");
+//        task.setJobName("Flylight Sample All Pipelines Task");
+//        task = c.saveOrUpdateTask(task);
+//        c.submitJob("GSPS_CompleteSamplePipeline", task.getObjectId());
+//        numSamplesReprocessed++;
+//    }
     numSamples++;
 }
 
 println "numSamples: "+numSamples
-println "numSamplesReprocessed: "+numSamplesReprocessed
+//println "numSamplesReprocessed: "+numSamplesReprocessed
 
 
