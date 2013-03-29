ac2b21dfd8499033c0554c1a0cc80630fd89af5a
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-03-29 13:44:16 +0000

    better algorithm for finding latest runs

--- compute/src/org/janelia/it/jacs/compute/service/align/ConfiguredPairAlignmentService.java
+++ compute/src/org/janelia/it/jacs/compute/service/align/ConfiguredPairAlignmentService.java
@@ -13,6 +13,7 @@ import org.janelia.it.jacs.model.entity.Entity;
 import org.janelia.it.jacs.model.entity.EntityConstants;
 import org.janelia.it.jacs.model.entity.cv.Objective;
 import org.janelia.it.jacs.model.user_data.Subject;
+import org.janelia.it.jacs.shared.utils.EntityUtils;
 
 /**
  * A configured aligner which takes additional parameters to align the 63x image to a whole brain 20x image.
@@ -80,40 +81,39 @@ public class ConfiguredPairAlignmentService extends ConfiguredAlignmentService {
         entityLoader.populateChildren(sample);
         for(Entity objectiveSample : sample.getChildren()) {
             
-            entityLoader.populateChildren(objectiveSample);
             String objective = objectiveSample.getValueByAttributeName(EntityConstants.ATTRIBUTE_OBJECTIVE);
-            String filename = objectiveSample.getValueByAttributeName(EntityConstants.ATTRIBUTE_DEFAULT_3D_IMAGE);
-            
             if (Objective.OBJECTIVE_20X.getName().equals(objective)) {
                 logger.info("Found 20x sub-sample: "+objectiveSample.getName());
-                if (filename!=null) {
-                    this.brain20xFilename = filename;
-                    logger.info("Found 20x aligned stack: "+brain20xFilename);
-                    Entity aligned = objectiveSample.getChildByAttributeName(EntityConstants.ATTRIBUTE_DEFAULT_3D_IMAGE);
-                    if (aligned!=null) {
+                Entity latest = getLatestResultOfType(objectiveSample, EntityConstants.TYPE_ALIGNMENT_RESULT);
+                if (latest != null) {
+                    String filename = latest.getValueByAttributeName(EntityConstants.ATTRIBUTE_DEFAULT_3D_IMAGE);
+                    if (filename!=null) {
+                        this.brain20xFilename = filename;
+                        logger.info("Found 20x aligned stack: "+brain20xFilename);
+                        Entity aligned = latest.getChildByAttributeName(EntityConstants.ATTRIBUTE_DEFAULT_3D_IMAGE);
+                        if (aligned==null) throw new IllegalStateException("No default 2d image for 20x alignment result: "+latest.getId());
                         String channelSpec = aligned.getValueByAttributeName(EntityConstants.ATTRIBUTE_CHANNEL_SPECIFICATION);
-                        if (channelSpec.contains("r")) {
-                            refChannel20xOneIndexed = channelSpec.indexOf('r') + 1;
-                            logger.info("Found 20x ref channel (one-indexed): "+refChannel20xOneIndexed);
-                        }
+                        if (channelSpec==null) throw new IllegalStateException("No chan spec for 20x aligned file: "+aligned.getId());
+                        this.refChannel20xOneIndexed = channelSpec.indexOf('r') + 1;
+                        logger.info("Found 20x ref channel (one-indexed): "+refChannel20xOneIndexed);
                     }
                 }
             }
             else if (Objective.OBJECTIVE_63X.getName().equals(objective)) {
                 logger.info("Found 63x sub-sample: "+objectiveSample.getName());
-                if (filename!=null) {
+                Entity latest = getLatestResultOfType(objectiveSample, EntityConstants.TYPE_SAMPLE_PROCESSING_RESULT);
+                if (latest!=null) {
+                    String filename = latest.getValueByAttributeName(EntityConstants.ATTRIBUTE_DEFAULT_3D_IMAGE);
                     this.inputFilename = filename;
-                    logger.info("Found 63x aligned stack: "+inputFilename);
-                    Entity aligned = objectiveSample.getChildByAttributeName(EntityConstants.ATTRIBUTE_DEFAULT_3D_IMAGE);
-                    if (aligned!=null) {
-                        this.channelSpec = aligned.getValueByAttributeName(EntityConstants.ATTRIBUTE_CHANNEL_SPECIFICATION);
-                        if (channelSpec.contains("r")) {
-                            this.refChannel = channelSpec.indexOf('r');
-                            this.refChannelOneIndexed = refChannel + 1;
-                            logger.info("Found 63x ref channel (one-indexed): "+refChannelOneIndexed);
-                            putOutputVars(channelSpec);
-                        }
-                    }
+                    logger.info("Found 63x stack: "+inputFilename);
+                    Entity aligned = latest.getChildByAttributeName(EntityConstants.ATTRIBUTE_DEFAULT_3D_IMAGE);
+                    if (aligned==null) throw new IllegalStateException("No default 2d image for 63x result: "+latest.getId());
+                    String channelSpec = aligned.getValueByAttributeName(EntityConstants.ATTRIBUTE_CHANNEL_SPECIFICATION);
+                    if (channelSpec==null) throw new IllegalStateException("No chan spec for 63x file: "+aligned.getId());
+                    this.refChannel = channelSpec.indexOf('r');
+                    this.refChannelOneIndexed = refChannel + 1;
+                    logger.info("Found 63x ref channel (one-indexed): "+refChannelOneIndexed);
+                    putOutputVars(channelSpec);
                 }
 
                 this.gender = sampleHelper.getConsensusLsmAttributeValue(objectiveSample, EntityConstants.ATTRIBUTE_GENDER, alignedArea);
@@ -128,6 +128,24 @@ public class ConfiguredPairAlignmentService extends ConfiguredAlignmentService {
                 }
             }
         }
+    }
+    
+    private Entity getLatestResultOfType(Entity objectiveSample, String resultType) throws Exception {
+        entityLoader.populateChildren(objectiveSample);
+        for(Entity pipelineRun : objectiveSample.getOrderedChildren()) {
+            entityLoader.populateChildren(pipelineRun);
+            if (pipelineRun.getEntityType().getName().equals(EntityConstants.TYPE_PIPELINE_RUN)) {
+                for(Entity a : pipelineRun.getChildren()) {
+                    entityLoader.populateChildren(a);
+                    if (a.getEntityType().getName().equals(resultType)) {
+                        if (EntityUtils.findChildWithType(a, EntityConstants.TYPE_ERROR) == null) {
+                            return a;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
     }
     
     @Override
