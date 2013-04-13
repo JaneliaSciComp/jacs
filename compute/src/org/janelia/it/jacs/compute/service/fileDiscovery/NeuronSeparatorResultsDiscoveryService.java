59367f9bb386dce7bda9a964cb497d464491a927
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-04-13 14:25:03 +0000

    fixed neuron separation null pointer

--- compute/src/org/janelia/it/jacs/compute/service/fileDiscovery/NeuronSeparatorResultsDiscoveryService.java
+++ compute/src/org/janelia/it/jacs/compute/service/fileDiscovery/NeuronSeparatorResultsDiscoveryService.java
@@ -30,8 +30,10 @@ public class NeuronSeparatorResultsDiscoveryService extends SupportingFilesDisco
     	this.objective = (String)processData.getItem("OBJECTIVE");
     	processData.putItem("RESULT_ENTITY_TYPE", EntityConstants.TYPE_NEURON_SEPARATOR_PIPELINE_RESULT);
     	
-    	String resultEntityName = (String)processData.getItem("RESULT_ENTITY_NAME")+" "+objective;
-    	processData.putItem("RESULT_ENTITY_NAME", resultEntityName);
+    	if (objective!=null) {
+        	String resultEntityName = (String)processData.getItem("RESULT_ENTITY_NAME")+" "+objective;
+        	processData.putItem("RESULT_ENTITY_NAME", resultEntityName);
+    	}
     	
     	super.execute(processData);
     }
