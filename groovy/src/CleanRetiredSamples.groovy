06afa3a64e24b56ec8785cfd110eb69acdc0e247
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-05-03 18:49:08 +0000

    new script for deleting unannotated retired samples

---
+++ groovy/src/CleanRetiredSamples.groovy
@@ -0,0 +1,44 @@
+import org.apache.solr.client.solrj.SolrQuery
+import org.apache.solr.common.SolrDocument
+import org.apache.solr.common.SolrDocumentList
+import org.janelia.it.jacs.model.entity.Entity
+import org.janelia.it.jacs.shared.utils.EntityUtils
+
+boolean DEBUG = false
+
+String ownerKey = "group:flylight"
+f = new JacsUtils(ownerKey, false)
+
+int numProcessed = 0
+toDelete = []
+
+println "INSPECTING SAMPLES:"
+
+for(Entity folder : f.e.getUserEntitiesByNameAndTypeName(ownerKey, "Retired Data", "Folder")) {
+    f.loadChildren(folder)
+    for(Entity sample : EntityUtils.getChildrenOfType(folder, "Sample")) {
+
+        sampleId = sample.id
+        SolrQuery query = new SolrQuery("(id:"+sampleId+" OR ancestor_ids:"+sampleId+") AND all_annotations:*")
+        SolrDocumentList results = f.s.search(null, query, false).response.results
+        int count = results.numFound
+
+        numProcessed++
+        println "  "+count+"\t"+sample.name+" "
+
+        if (count==0) {
+            toDelete.add(sample)
+        }
+    }
+}
+
+println "Processed "+numProcessed+" samples. Found "+toDelete.size()+" candidate samples for deletion"
+if (!DEBUG) {
+    println "DELETING SAMPLES:"
+    for(Entity sample in toDelete) {
+        f.e.deleteEntityTree(sample.ownerKey, sample.id)
+    }
+}
+
+println "Done"
+
