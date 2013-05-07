7eafbeeb8618733c818b6bdfc1d967d9d9239bb9
Sean Murphy <murphys@janelia.hhmi.org>
2013-05-07 17:19:22 +0000

    Added root geo tree attribute.

--- model/src/org/janelia/it/jacs/model/entity/EntityConstants.java
+++ model/src/org/janelia/it/jacs/model/entity/EntityConstants.java
@@ -132,6 +132,7 @@ public class EntityConstants {
     //  "<id>:<parentId>:<index>:<x,y,z>:<comment>"
     //
     public static final String ATTRIBUTE_GEO_TREE_COORDINATE                 = "Geometric Tree Coordinate";
+    public static final String ATTRIBUTE_GEO_ROOT_ID                         = "Root Geometric Tree Element";
 
     // Alignment Spaces
     public static final String ATTRIBUTE_ALIGNMENT_SPACE                     = "Alignment Space"; // e.g. "Unified 20x Alignment Space"
