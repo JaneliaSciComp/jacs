6c45d3f6cd5f4757c4c2edd89d0860c52e6ea049
Todd Safford <saffordt@janelia.hhmi.org>
2013-05-14 14:30:04 +0000

    Silencing the memory check that was causing Scientific Linux to segfault Once in place, everything seems to work perfectly so far

--- buildprocess/deployment/templates/linux_template/start.sh
+++ buildprocess/deployment/templates/linux_template/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export MALLOC_CHECK_=0
 DIR=$(cd "$(dirname "$0")"; pwd)
 cd $DIR
 java -Dsun.awt.disableMixing=true -XX:+UseParallelGC -XX:-UseGCOverheadLimit -jar workstation.jar -Xms512m -Xmx2560m &
