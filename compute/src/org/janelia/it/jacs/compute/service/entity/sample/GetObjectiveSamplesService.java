ba3b6399b8075b30aaf53a0bcd2abf4c63c402e6
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-05-24 18:01:48 +0000

    changed polarity pipeline to work on 20x only samples

--- compute/src/org/janelia/it/jacs/compute/service/entity/sample/GetObjectiveSamplesService.java
+++ compute/src/org/janelia/it/jacs/compute/service/entity/sample/GetObjectiveSamplesService.java
@@ -1,10 +1,13 @@
 package org.janelia.it.jacs.compute.service.entity.sample;
 
+import java.util.List;
+
 import org.janelia.it.jacs.compute.service.entity.AbstractEntityService;
 import org.janelia.it.jacs.model.entity.Entity;
 import org.janelia.it.jacs.model.entity.EntityConstants;
 import org.janelia.it.jacs.model.entity.cv.Objective;
 import org.janelia.it.jacs.shared.utils.EntityUtils;
+
 
 /**
  * Extracts sub-samples based on their objective.
@@ -46,56 +49,67 @@ public class GetObjectiveSamplesService extends AbstractEntityService {
     	}
     	
     	logger.info("Retrieved sample: "+sampleEntity.getName()+" (id="+sampleEntityId+")");
-    	
     	populateChildren(sampleEntity);
-    	
-    	logger.info("Putting '"+sampleEntity.getId()+"' in PARENT_SAMPLE_ID");
-        processData.putItem("PARENT_SAMPLE_ID", sampleEntity.getId().toString());
+        List<Entity> subsamples = EntityUtils.getChildrenOfType(sampleEntity, EntityConstants.TYPE_SAMPLE);
         
-    	for(Entity subsample : EntityUtils.getChildrenOfType(sampleEntity, EntityConstants.TYPE_SAMPLE)) {
-    	    String objective = subsample.getValueByAttributeName(EntityConstants.ATTRIBUTE_OBJECTIVE);
-    	    if (Objective.OBJECTIVE_20X.getName().equals(objective)) {
+        if (!subsamples.isEmpty()) {
 
-    	        if (reuse20xAlignment) {
-    	            
-    	            Entity latest20x = null;
-    	            populateChildren(subsample);
-    	            for(Entity pipelineRun : subsample.getOrderedChildren()) {
-                        populateChildren(pipelineRun);
-    	                if (pipelineRun.getEntityType().getName().equals(EntityConstants.TYPE_PIPELINE_RUN)) {
-    	                    for(Entity a : pipelineRun.getChildren()) {
-    	                        populateChildren(a);
-    	                        if (a.getEntityType().getName().equals(EntityConstants.TYPE_ALIGNMENT_RESULT)) {
-    	                            if (EntityUtils.findChildWithType(a, EntityConstants.TYPE_ERROR) == null) {
-    	                                latest20x = a;    
-    	                            }
-    	                        }
-    	                    }
-    	                }
-    	            }
-    	            
-    	            if (latest20x == null) {
-    	                logger.info("Could not find existing 20x alignment for reuse");
-    	                logger.info("Putting '"+subsample.getId()+"' in SAMPLE_20X_ID");
+            logger.info("Putting '"+sampleEntity.getId()+"' in PARENT_SAMPLE_ID");
+            processData.putItem("PARENT_SAMPLE_ID", sampleEntity.getId().toString());
+            
+        	for(Entity subsample : subsamples) {
+        	    String objective = subsample.getValueByAttributeName(EntityConstants.ATTRIBUTE_OBJECTIVE);
+        	    if (Objective.OBJECTIVE_20X.getName().equals(objective)) {
+    
+        	        if (reuse20xAlignment) {
+        	            
+        	            Entity latest20x = null;
+        	            populateChildren(subsample);
+        	            for(Entity pipelineRun : subsample.getOrderedChildren()) {
+                            populateChildren(pipelineRun);
+        	                if (pipelineRun.getEntityType().getName().equals(EntityConstants.TYPE_PIPELINE_RUN)) {
+        	                    for(Entity a : pipelineRun.getChildren()) {
+        	                        populateChildren(a);
+        	                        if (a.getEntityType().getName().equals(EntityConstants.TYPE_ALIGNMENT_RESULT)) {
+        	                            if (EntityUtils.findChildWithType(a, EntityConstants.TYPE_ERROR) == null) {
+        	                                latest20x = a;    
+        	                            }
+        	                        }
+        	                    }
+        	                }
+        	            }
+        	            
+        	            if (latest20x == null) {
+        	                logger.info("Could not find existing 20x alignment for reuse");
+        	                logger.info("Putting '"+subsample.getId()+"' in SAMPLE_20X_ID");
+                            processData.putItem("SAMPLE_20X_ID", subsample.getId().toString());
+        	            }
+        	            else {
+        	                logger.info("Reusing existing 20x alignment: id="+latest20x.getId());
+        	            }
+        	        }
+        	        else {
+                        logger.info("Putting '"+subsample.getId()+"' in SAMPLE_20X_ID");
                         processData.putItem("SAMPLE_20X_ID", subsample.getId().toString());
-    	            }
-    	            else {
-    	                logger.info("Reusing existing 20x alignment: id="+latest20x.getId());
-    	            }
-    	        }
-    	        else {
-                    logger.info("Putting '"+subsample.getId()+"' in SAMPLE_20X_ID");
-                    processData.putItem("SAMPLE_20X_ID", subsample.getId().toString());
-    	        }
-    	    }
-    	    else if (Objective.OBJECTIVE_40X.getName().equals(objective)) {
-    	        logger.info("Putting '"+subsample.getId()+"' in SAMPLE_40X_ID");
-    	        processData.putItem("SAMPLE_40X_ID", subsample.getId().toString());
+        	        }
+        	    }
+        	    else if (Objective.OBJECTIVE_40X.getName().equals(objective)) {
+        	        logger.info("Putting '"+subsample.getId()+"' in SAMPLE_40X_ID");
+        	        processData.putItem("SAMPLE_40X_ID", subsample.getId().toString());
+                }
+        	    else if (Objective.OBJECTIVE_63X.getName().equals(objective)) {
+        	        logger.info("Putting '"+subsample.getId()+"' in SAMPLE_63X_ID");
+        	        processData.putItem("SAMPLE_63X_ID", subsample.getId().toString());
+                }
+        	}
+        }
+        else {
+            logger.info("No subsamples found");
+            String objective = sampleEntity.getValueByAttributeName(EntityConstants.ATTRIBUTE_OBJECTIVE);
+            if (objective!=null) {
+                logger.info("Putting the parent sample '"+sampleEntity.getId()+"' in SAMPLE_20X_ID");
+                processData.putItem("SAMPLE_"+objective.toUpperCase()+"_ID", sampleEntity.getId().toString());
             }
-    	    else if (Objective.OBJECTIVE_63X.getName().equals(objective)) {
-    	        logger.info("Putting '"+subsample.getId()+"' in SAMPLE_63X_ID");
-    	        processData.putItem("SAMPLE_63X_ID", subsample.getId().toString());
-            }
-    	}
+        }
     }
 }
