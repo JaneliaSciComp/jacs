4b48528ee3538271be3fa116d21e94be11c3f23e
Les Foster <fosterl@janelia.hhmi.org>
2013-05-30 18:27:45 +0000

    Added an attribute of "Render Method" to Aligned Item objects.  Using same to enable a user menu item for allowing "passthrough rendering" (or raw rendering) from the Layers panel.

--- model/src/org/janelia/it/jacs/model/entity/EntityConstants.java
+++ model/src/org/janelia/it/jacs/model/entity/EntityConstants.java
@@ -135,6 +135,7 @@ public class EntityConstants {
     public static final String ATTRIBUTE_ITEM                                = "Item";            // The actual entity item wrapped by an Aligned Item
     public static final String ATTRIBUTE_VISIBILITY                          = "Visibility";      // e.g. "true"
     public static final String ATTRIBUTE_COLOR                               = "Color";           // RGBA hex format, e.g. "ff0000ff"
+    public static final String ATTRIBUTE_RENDER_METHOD                       = "Render Method";   // Special overrides to normal rendering.  Ex: Passthrough
 
     public static final String ATTRIBUTE_MASK_IMAGE                          = "Mask Image";
     public static final String ATTRIBUTE_CHAN_IMAGE                          = "Chan Image";
