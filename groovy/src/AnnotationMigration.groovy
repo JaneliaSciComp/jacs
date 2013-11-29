c139bb5e23e63bc1f87f930460b0345891e73909
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-11-29 17:30:08 +0000

    many improvements

--- groovy/src/AnnotationMigration.groovy
+++ groovy/src/AnnotationMigration.groovy
@@ -1,77 +1,89 @@
 import com.google.common.collect.Iterables
 import org.apache.solr.client.solrj.SolrQuery
 import org.apache.solr.common.SolrDocumentList
+import org.janelia.it.FlyWorkstation.gui.framework.session_mgr.SessionMgr
 import org.janelia.it.FlyWorkstation.shared.util.WorkstationFile
 import org.janelia.it.jacs.model.entity.Entity
 import org.janelia.it.jacs.model.entity.EntityConstants
 import org.janelia.it.jacs.shared.utils.EntityUtils
+import org.janelia.it.jacs.shared.utils.StringUtils
 
 class AnnotationMigrationScript {
     static final OWNER = "nerna"
     static final OWNER_KEY = "user:"+OWNER
     static final OUTPUT_FILE = "annot_migration.txt"
+    static final ADD_ATTRS = true
 
-    //System.out//
     PrintStream file = new PrintStream(OUTPUT_FILE)
     JacsUtils f = new JacsUtils(SampleReportConstants.OWNER_KEY, true)
     List<MigrationCase> allMcs = new ArrayList<>()
 
     def main() {
 
-//        processSample(f.e.getEntityById(OWNER_KEY, 1903189566282530914L))
-//        processSample(f.e.getEntityById(OWNER_KEY, 1679282453644050530L))
-//        processSample(f.e.getEntityById(OWNER_KEY, 1889491941918244962L))
-//        processSample(f.e.getEntityById(OWNER_KEY, 1679282498829287522L))
+        try {
+            def samples = f.e.getUserEntitiesByTypeName(SampleAnnotationsConstants.OWNER_KEY, "Sample")
+            println "Ordering "+samples.size()+" samples..."
 
+            List<Entity> orderedSamples = new ArrayList<Entity>(samples);
+            Collections.sort(orderedSamples, new Comparator<Entity>() {
+                int compare(Entity o1, Entity o2) {
+                    return o1.name.compareTo(o2.name)
+                }
+            })
 
-        def samples = f.e.getUserEntitiesByTypeName(SampleAnnotationsConstants.OWNER_KEY, "Sample")
-        List<Entity> orderedSamples = new ArrayList<Entity>(samples);
-        Collections.sort(orderedSamples, new Comparator<Entity>() {
-            int compare(Entity o1, Entity o2) {
-                return o1.name.compareTo(o2.name)
+            println "Processing "+orderedSamples.size()+" samples..."
+            for(Entity sample : orderedSamples) {
+                processSample(sample)
             }
-        })
-
-        for(Entity sample : orderedSamples) {
-            processSample(sample)
+        }
+        catch (Throwable t) {
+            t.printStackTrace()
         }
 
-        generateReport(true)
-        generateReport(false)
+        generateReport(true, false)
+        generateReport(false, false)
+        generateReport(true, true)
+        generateReport(false, true)
 
         System.exit(0)
     }
 
-    def generateReport(boolean annotatedSource) {
+    def generateReport(boolean annotatedSource, boolean aligned) {
 
         int total = 0
         int totalProb = 0
         int numWarped = 0
         int numMapped = 0
         for(MigrationCase mc : allMcs) {
+            if (aligned && (!mc.source.separationName.startsWith("Aligned") && !mc.target.separationName.startsWith("Aligned"))) continue
+            if (!aligned && (mc.source.separationName.startsWith("Aligned") || mc.target.separationName.startsWith("Aligned"))) continue
             if (annotatedSource && mc.source.numAnnotated<=0) continue
             if (!annotatedSource && mc.source.numAnnotated>0) continue
             if (mc.target.warped) {
                 numWarped++
             }
-            else if (mc.target.mapping!=null) {
+            else if (mc.target.hasMapping) {
                 numMapped++
             }
             totalProb += mc.successProbability
             total++
         }
 
-        def name = annotatedSource ? "Annotated" : "Unannotated"
+        def annotationStr = annotatedSource ? "Annotated" : "Unannotated"
+        def alignmentStr = aligned ? "Aligned" : "Unaligned"
 
         file.println "-------------------------------------------------------"
-        file.println name+" Separation Migration Report"
+        file.println annotationStr+" "+alignmentStr+" Separation Migration Report"
         file.println "Total separations: "+total
         file.println "Warped separations: "+numWarped
         file.println "Mapped separations: "+numMapped
-        file.println "Average success probability: "+String.format('%.2f',100*totalProb/total)+"%"
+        if (total>0) {
+            file.println "Average success probability: "+String.format('%.2f',100*totalProb/total)+"%"
+        }
     }
 
     def processSample(Entity sample) {
+        println "Processing "+sample.name
         f.loadChildren(sample)
 
         if (sample.name.contains("~")) return
@@ -95,13 +107,12 @@ class AnnotationMigrationScript {
 
     def processTrueSample(String data_set, Entity sample) {
 
-//        def annotations = getAnnotations(sample.id)
-//        if (annotations.isEmpty()) return // Annotated samples only
-
         List<MigrationCase> mcs = new ArrayList<>()
 
         file.println()
         file.println(sample.name)
+
+        def lastUnalignedSepForRun = null
 
         for(Entity pipelineRun : EntityUtils.getChildrenOfType(sample, "Pipeline Run")) {
             f.loadChildren(pipelineRun)
@@ -136,27 +147,50 @@ class AnnotationMigrationScript {
 
                     Separation s = new Separation()
                     s.id = separation.id
-                    s.sampleName = sample.name
                     s.separationName = separation.name
+                    s.sampleName = sample.name
                     s.dataSetName = data_set
                     s.numNeurons = neuronCollection.children.size()
                     s.numAnnotated = annotatedNeurons.size()
                     s.mapping = getMapping(separation)
                     s.warped = getWarped(separation)
+                    s.hasMapping = (s.mapping != null)
 
                     int mapCount = s.mapping==null?0:s.mapping.size()
-                    def isWarped = s.warped?", warped":""
-                    file.println("            "+separation.name+" (total="+s.numNeurons+", annotated="+s.numAnnotated+", mapped="+mapCount+isWarped+")")
+
+                    def attrs = []
+                    if (s.numNeurons>0) {
+                        attrs.add("total="+s.numNeurons)
+                    }
+                    if (s.numAnnotated>0) {
+                        attrs.add("annotated="+s.numAnnotated)
+                    }
+                    if (mapCount>0) {
+                        attrs.add("mapped="+mapCount)
+                    }
+                    if (s.warped) {
+                        attrs.add("warped=true")
+                    }
+
+                    file.println "            "+separation.name+" ("+StringUtils.getCommaDelimited(attrs)+")"
 
                     resultSeps.add(s)
 
                     if (result.entityType.name==EntityConstants.TYPE_SAMPLE_PROCESSING_RESULT) {
                         lastUnalignedSep = s
+                        if (lastUnalignedSepForRun!=null) {
+                            // Migrate between unaligned separations of multiple runs
+                            MigrationCase mc = new MigrationCase()
+                            mc.source = lastUnalignedSepForRun
+                            mc.target = lastUnalignedSep
+                            mcs.add(mc)
+                        }
                     }
                 }
 
                 if (resultSeps.size()>1) {
                     for(int i=1; i<resultSeps.size()-1; i++) {
+                        // Migrate between multiple separations of the same result
                         MigrationCase mc = new MigrationCase()
                         mc.source = resultSeps[i-1]
                         mc.target = resultSeps[i]
@@ -167,6 +201,7 @@ class AnnotationMigrationScript {
                 if (result.entityType.name==EntityConstants.TYPE_ALIGNMENT_RESULT) {
                     if (lastUnalignedSep!=null) {
                         for(Separation als : resultSeps) {
+                            // Migrate between unaligned and aligned separations in the same run
                             MigrationCase mc = new MigrationCase()
                             mc.source = lastUnalignedSep
                             mc.target = als
@@ -177,39 +212,52 @@ class AnnotationMigrationScript {
                         file.println "Alignment has no source: "+result.id
                     }
                 }
+
+                lastUnalignedSepForRun = lastUnalignedSep
             }
         }
 
         file.println "Migrations:"
         for(MigrationCase mc : mcs) {
+
+            if (ADD_ATTRS) {
+                f.e.addEntityToParent(OWNER_KEY, mc.target.id, mc.source.id, null, EntityConstants.ATTRIBUTE_SOURCE_SEPARATION)
+                if (mc.target.warped) {
+                    f.e.setOrUpdateValue(OWNER_KEY, mc.target.id, EntityConstants.ATTRIBUTE_IS_WARPED_SEPARATION, EntityConstants.ATTRIBUTE_IS_WARPED_SEPARATION)
+                }
+            }
+
             if (mc.target.warped) {
                 if (mc.source.numNeurons<=mc.target.numNeurons) {
-                    mc.successProbability = mc.source.numNeurons / mc.target.numNeurons
+                    mc.successProbability = mc.target.numNeurons==0?1:(mc.source.numNeurons / mc.target.numNeurons)
                 }
                 else {
-                    mc.successProbability = mc.target.numNeurons / mc.source.numNeurons
+                    mc.successProbability = mc.source.numNeurons==0?1:(mc.target.numNeurons / mc.source.numNeurons)
                 }
             }
             else {
                 int mapCount = mc.target.mapping==null?0:+mc.target.mapping.size()
-                mc.successProbability = mapCount / mc.source.numNeurons
+                mc.successProbability =  mc.source.numNeurons==0?1:(mapCount / mc.source.numNeurons)
             }
 
             file.println mc.source.separationName+" -> "+mc.target.separationName+" ("+String.format('%.2f',mc.successProbability*100)+"%)"
         }
 
-        allMcs.addAll(mcs)
+        for(MigrationCase mc : mcs) {
+            mc.source.mapping = null // Save memory
+            mc.target.mapping = null
+            allMcs.add(mc)
+        }
     }
 
     def Map<Integer,Integer> getMapping(Entity separation) {
         String dir = separation.getValueByAttributeName(EntityConstants.ATTRIBUTE_FILE_PATH)
         File mappingFile = new File(dir,"mapping_issues.txt")
-        WorkstationFile file = new WorkstationFile(mappingFile.absolutePath)
+        Scanner scanner = null
+
         try {
-            file.get()
-            if (file.statusCode!=200) return null
-            InputStream is = file.getStream()
-            Scanner scanner = new Scanner(is).useDelimiter("\\n")
+            File cachedFile = SessionMgr.getCachedFile(mappingFile.getAbsolutePath(), false)
+            scanner = new Scanner(cachedFile).useDelimiter("\\n")
             Map<Integer,Integer> mapping = new HashMap<>()
 
             while (scanner.hasNextLine()) {
@@ -226,25 +274,25 @@ class AnnotationMigrationScript {
                     }
                 }
             }
-            scanner.close()
-            is.close()
             return mapping
         }
         catch (Exception e) {
-            System.out.println("            Could not read mapping file: "+e.getMessage());
+            println "  Could not read mapping file: "+e.getMessage()
             return null
+        }
+        finally {
+            if (scanner!=null) scanner.close()
         }
     }
 
     def boolean getWarped(Entity separation) {
         String dir = separation.getValueByAttributeName(EntityConstants.ATTRIBUTE_FILE_PATH)
-        File configFile = new File(dir+"/sge_config","neuSepConfiguration.1")
+        Scanner scanner = null
 
-        WorkstationFile file = new WorkstationFile(configFile.absolutePath)
         try {
-            file.get()
-            InputStream is = file.getStream()
-            Scanner scanner = new Scanner(is).useDelimiter("\\n")
+            File configFile = new File(dir+"/sge_config","neuSepConfiguration.1")
+            File cachedFile = SessionMgr.getCachedFile(configFile.getAbsolutePath(), false)
+            scanner = new Scanner(cachedFile).useDelimiter("\\n")
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine().trim();
                 if (line =~ /NeuronAligned/) {
@@ -253,20 +301,24 @@ class AnnotationMigrationScript {
             }
         }
         catch (Exception e) {
-            System.out.println("            Could not read config file: "+e.getMessage());
+            println "  Could not read config file: "+e.getMessage()
+        }
+        finally {
+            if (scanner!=null) scanner.close()
         }
         return false
     }
 
     class Separation {
+        Long id
         String sampleName
         String separationName
         String dataSetName
-        Long id
         int numNeurons
         int numAnnotated
-        Map<Integer,Integer> mapping = null
         boolean warped = false
+        boolean hasMapping = false
+        Map<Integer,Integer> mapping = null
     }
 
     class MigrationCase {
