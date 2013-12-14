39ed2cfef7379c1e69a682287bb58a237eacce13
Konrad Rokicki <rokickik@janelia.hhmi.org>
2013-12-14 18:46:51 +0000

    support bzip2 and gzip compression

--- compute/scripts/single_neuron/ImageTools/archiveSync.sh
+++ compute/scripts/single_neuron/ImageTools/archiveSync.sh
@@ -9,6 +9,18 @@ SOURCE_NAME=`basename "$SOURCE_FILE"`
 TARGET_NAME=`basename "$TARGET_FILE"`
 SOURCE_DIR=`dirname "$SOURCE_FILE"`
 TARGET_DIR=`dirname "$TARGET_FILE"`
+
+echo "Source: $SOURCE_FILE"
+echo "Target: $TARGET_FILE"
+
+TMP_FILENAME="tmp"
+SOURCE_EXT=${SOURCE_FILE#*.}
+if [ $SOURCE_EXT == "lsm.bz2" ]; then
+    TMP_FILENAME="tmp.bz2"
+elif [ $SOURCE_EXT == "lsm.gz" ]; then
+    TMP_FILENAME="tmp.gz"
+fi
+
 echo "Target dir: $TARGET_DIR"
 if [ -s "$TARGET_FILE" ] && [ "$SOURCE_NAME" == "$TARGET_NAME" ]; then
     echo "Target already exists, using rsync"
@@ -23,10 +35,16 @@ else
     export TMPDIR="$TARGET_DIR"
     TEMP_DIR=`mktemp -d`
     echo "Copying source to temporary target: $TEMP_DIR"
-    cp -a "$SOURCE_FILE" "$TEMP_DIR"/tmp
+    cp -a "$SOURCE_FILE" "$TEMP_DIR"/$TMP_FILENAME
+    if [ $SOURCE_EXT == "lsm.bz2" ]; then
+        echo "Decompressing bzip2 file"
+        bzip2 -d "$TEMP_DIR/$TMP_FILENAME"
+    elif [ $SOURCE_EXT == "lsm.gz" ]; then
+        echo "Decompressing gzip file"
+        gzip -d "$TEMP_DIR/$TMP_FILENAME"
+    fi
     echo "Moving into final location: $TARGET_FILE"
     mv "$TEMP_DIR"/tmp "$TARGET_FILE"
     rm -rf "$TEMP_DIR"
 fi
-
 
