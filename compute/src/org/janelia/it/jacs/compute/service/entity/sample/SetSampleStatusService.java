6672849f7f60028bb24f8a754cfd09ed62e4d123
Konrad Rokicki <rokickik@janelia.hhmi.org>
2014-02-10 21:02:58 +0000

    parent samples are not set to completed until all children are complete

---
+++ compute/src/org/janelia/it/jacs/compute/service/entity/sample/SetSampleStatusService.java
@@ -0,0 +1,46 @@
+package org.janelia.it.jacs.compute.service.entity.sample;
+
+import org.janelia.it.jacs.compute.service.entity.AbstractEntityService;
+import org.janelia.it.jacs.model.entity.Entity;
+import org.janelia.it.jacs.model.entity.EntityConstants;
+import org.janelia.it.jacs.shared.utils.EntityUtils;
+
+/**
+ * Sets the status attribute of the sample. 
+ *   
+ * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
+ */
+public class SetSampleStatusService extends AbstractEntityService {
+
+    public void execute() throws Exception {
+        
+        String status = data.getRequiredItemAsString("STATUS");
+    	Long entityId = data.getRequiredItemAsLong("ENTITY_ID");
+    	Entity sample = entityBean.getEntityById(entityId);
+    	Entity parentSample = entityBean.getAncestorWithType(sample, EntityConstants.TYPE_SAMPLE);
+    	
+    	if (parentSample==null) {
+            logger.info("Setting status to "+status+" on parent-less sample "+sample.getName()+" (id="+entityId+")");
+            entityBean.setOrUpdateValue(entityId, EntityConstants.ATTRIBUTE_STATUS, status);
+    	}
+    	else {
+            logger.info("Setting status to "+status+" on sample "+sample.getName()+" (id="+entityId+")");
+            entityBean.setOrUpdateValue(entityId, EntityConstants.ATTRIBUTE_STATUS, status);
+            
+            if (status.equals(EntityConstants.VALUE_COMPLETE)) {
+                entityLoader.populateChildren(parentSample);
+                for(Entity childSample : EntityUtils.getChildrenOfType(parentSample, EntityConstants.TYPE_SAMPLE)) {
+                    if (!EntityConstants.VALUE_COMPLETE.equals(childSample.getValueByAttributeName(EntityConstants.ATTRIBUTE_STATUS))) {
+                        // All child samples are not finished, so the parent cannot be finished
+                        return;
+                    }
+                }
+            }
+            
+            // Also update parent status
+            logger.info("Setting status to "+status+" on parent sample "+sample.getName()+" (id="+entityId+")");
+            entityBean.setOrUpdateValue(parentSample.getId(), EntityConstants.ATTRIBUTE_STATUS, status);
+            
+    	}
+    }
+}
