61c4af57f2c35b271a62bdb4164f6f763b99f0fd
Konrad Rokicki <rokickik@janelia.hhmi.org>
2014-04-28 13:37:07 -0400

    added screen data moved images into maps efficient annotation load

--- model/src/org/janelia/it/jacs/model/domain/HasMips.java
+++
@@ -1,9 +0,0 @@
-package org.janelia.it.jacs.model.domain;
-
-public interface HasMips {
-
-    public abstract String getSignalMipFilepath();
-
-    public abstract String getReferenceMipFilepath();
-
-}
--- model/src/org/janelia/it/jacs/model/domain/SampleTile.java
+++ model/src/org/janelia/it/jacs/model/domain/SampleTile.java
@@ -1,54 +1,48 @@
 package org.janelia.it.jacs.model.domain;
 
 import java.util.List;
+import java.util.Map;
 
-public class SampleTile implements HasMips {
+public class SampleTile implements HasImages {
     
     private String name;
     private String anatomicalArea;
     private List<Reference> lsmReferences;
-    private String signalMipFilepath;
-    private String referenceMipFilepath;
+    private ReverseReference masks;
+    private Map<ImageType,String> images;
 
     /* EVERYTHING BELOW IS AUTO-GENERATED */
     
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-    
     public String getAnatomicalArea() {
         return anatomicalArea;
     }
-
     public void setAnatomicalArea(String anatomicalArea) {
         this.anatomicalArea = anatomicalArea;
     }
-
     public List<Reference> getLsmReferences() {
         return lsmReferences;
     }
-
     public void setLsmReferences(List<Reference> lsmReferences) {
         this.lsmReferences = lsmReferences;
     }
-
-    public String getSignalMipFilepath() {
-        return signalMipFilepath;
+    public ReverseReference getMasks() {
+        return masks;
     }
-
-    public void setSignalMipFilepath(String signalMipFilepath) {
-        this.signalMipFilepath = signalMipFilepath;
+    public void setMasks(ReverseReference masks) {
+        this.masks = masks;
     }
-
-    public String getReferenceMipFilepath() {
-        return referenceMipFilepath;
+    public Map<ImageType, String> getImages() {
+        return images;
     }
-
-    public void setReferenceMipFilepath(String referenceMipFilepath) {
-        this.referenceMipFilepath = referenceMipFilepath;
+    public void setImages(Map<ImageType, String> images) {
+        this.images = images;
     }
+    
+    
 }
