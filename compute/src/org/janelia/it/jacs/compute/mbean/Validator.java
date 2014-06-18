7b86b13994eb23a7c4c6c131f2e13553cbf45061
lesfoster <fosterl@janelia.hhmi.org>
2014-06-18 09:21:51 -0400

    Added simplistic validation service / mbean.

---
+++ compute/src/org/janelia/it/jacs/compute/mbean/Validator.java
@@ -0,0 +1,7 @@
+package org.janelia.it.jacs.compute.mbean;
+
+/**
+ * Created by fosterl on 6/17/14.
+ */
+public class Validator {
+}
---
+++ compute/src/org/janelia/it/jacs/compute/service/validation/SimpleValidationService.java
@@ -0,0 +1,7 @@
+package org.janelia.it.jacs.compute.service.validation;
+
+/**
+ * Created by fosterl on 6/17/14.
+ */
+public class SimpleValidationService {
+}
