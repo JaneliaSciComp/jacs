1eb801a06d82fceaed8d3af9d66130c83c2876a0
lesfoster <fosterl@janelia.hhmi.org>
2014-06-18 12:29:54 -0400

    Missing code overlooked earlier??

--- compute/src/org/janelia/it/jacs/compute/mbean/Validator.java
+++ compute/src/org/janelia/it/jacs/compute/mbean/Validator.java
@@ -1,7 +1,46 @@
 package org.janelia.it.jacs.compute.mbean;
 
+import org.apache.log4j.Logger;
+import org.janelia.it.jacs.compute.api.EJBFactory;
+import org.janelia.it.jacs.model.tasks.Event;
+import org.janelia.it.jacs.model.tasks.TaskParameter;
+import org.janelia.it.jacs.model.tasks.utility.GenericTask;
+import org.janelia.it.jacs.model.user_data.Node;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
+ * Beginnings of a validation launcher.
+ *
  * Created by fosterl on 6/17/14.
  */
-public class Validator {
+public class Validator implements ValidatorMBean {
+    private Logger log = Logger.getLogger(Validator.class);
+    @Override
+    public void runValidations(String user) {
+        try {
+            String processName = "ValidationServicePipeline";
+            String displayName = "Simplistic NoOp Validation";
+            //NO PARAMS YET
+            HashSet<TaskParameter> taskParameters = new HashSet<TaskParameter>();
+            saveAndRunTask(user, processName, displayName, taskParameters);
+        } catch ( Exception ex ) {
+            log.error("Failed to complete validation", ex);
+        }
+    }
+
+    // Borrowed from SampleDataManager
+    private void saveAndRunTask(String user, String processName, String displayName, HashSet<TaskParameter> parameters) throws Exception {
+        GenericTask task = new GenericTask(new HashSet<Node>(), user, new ArrayList<Event>(),
+                parameters, processName, displayName);
+        saveAndRunTask(task);
+    }
+
+    // Borrowed from SampleDataManager
+    private void saveAndRunTask(GenericTask task) throws Exception {
+        task = (GenericTask) EJBFactory.getLocalComputeBean().saveOrUpdateTask(task);
+        EJBFactory.getLocalComputeBean().submitJob(task.getTaskName(), task.getObjectId());
+    }
+
 }
--- compute/src/org/janelia/it/jacs/compute/mbean/ValidatorMBean.java
+++ compute/src/org/janelia/it/jacs/compute/mbean/ValidatorMBean.java
@@ -1,7 +1,9 @@
 package org.janelia.it.jacs.compute.mbean;
 
 /**
+ * MBean-required artifact: the interface for validation.
  * Created by fosterl on 6/17/14.
  */
 public interface ValidatorMBean {
+    void runValidations(String user);
 }
