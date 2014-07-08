05252602f105941a194e360a1740bc04b17999e4
Les Foster <fosterl@janalia.hhmi.org>
2014-07-08 13:36:34 -0400

    Validating presence of certain attributes on Neuron Separator Pipeline Results.

--- compute/src/org/janelia/it/jacs/compute/process_result_validation/content_checker/type_validator/NeuronSeparatorPipelineResultValidator.java
+++ compute/src/org/janelia/it/jacs/compute/process_result_validation/content_checker/type_validator/NeuronSeparatorPipelineResultValidator.java
@@ -16,9 +16,16 @@ public class NeuronSeparatorPipelineResultValidator implements TypeValidator {
     private EntityBeanLocal entityBean;
     private FileValidator fileValidator;
 
-    private static final ValidationLogger.Category NO_FRAGMENTS = new ValidationLogger.Category("No Fragments");
-    private static final ValidationLogger.Category NO_DEFAULT_3D_IMAGE = new ValidationLogger.Category("No Default 3D Image");
-    private static final ValidationLogger.Category NO_FAST_LOAD_3D_IMAGE = new ValidationLogger.Category("No Fast-Load 3D Image");
+    private static final ValidationLogger.Category NO_FRAGMENTS = new ValidationLogger.Category("Missing Fragments");
+    private static final ValidationLogger.Category NO_DEFAULT_3D_IMAGE = new ValidationLogger.Category("Missing Default 3D Image");
+    private static final ValidationLogger.Category NO_FAST_LOAD_3D_IMAGE = new ValidationLogger.Category("Missing Fast-Load 3D Image");
+
+    private static final ValidationLogger.Category NO_OBJECTIVE_ATTRIBUTE = new ValidationLogger.Category("Missing Objective Attribute");
+    private static final ValidationLogger.Category NO_PIXRES_ATTRIBUTE = new ValidationLogger.Category("Missing Pixel Resolution Attribute");
+    private static final ValidationLogger.Category NO_OPTICALRES_ATTRIBUTE = new ValidationLogger.Category("Missing Optical Resolution Attribute");
+
+    @SuppressWarnings("unused")
+    private static final ValidationLogger.Category FOOLS_ERRAND = new ValidationLogger.Category("Missing No-Such Attribute");
 
     private static final String ERROR_RPT_FMT = "No %s found for %s: %d.";
 
@@ -29,17 +36,21 @@ public class NeuronSeparatorPipelineResultValidator implements TypeValidator {
         this.validationLogger.addCategory( NO_FRAGMENTS );
         this.validationLogger.addCategory( NO_DEFAULT_3D_IMAGE );
         this.validationLogger.addCategory( NO_FAST_LOAD_3D_IMAGE );
+
+        this.validationLogger.addCategory( NO_OBJECTIVE_ATTRIBUTE );
+        this.validationLogger.addCategory( NO_OPTICALRES_ATTRIBUTE );
+        this.validationLogger.addCategory( NO_PIXRES_ATTRIBUTE );
+
+        //DEBUG: this.validationLogger.addCategory( FOOLS_ERRAND );
     }
 
     @Override
     public void validate(Entity entity, Long sampleId) throws Exception {
-        Entity fragments = entity.getChildByAttributeName(EntityConstants.ATTRIBUTE_MASK_ENTITY_COLLECTION);
-        if ( fragments == null ) {
-            validationLogger.reportError(
-                    sampleId, entity.getId(), entity.getEntityTypeName(), NO_FRAGMENTS,
-                    String.format( ERROR_RPT_FMT, EntityConstants.ATTRIBUTE_MASK_ENTITY_COLLECTION, EntityConstants.TYPE_NEURON_SEPARATOR_PIPELINE_RESULT, entity.getId() ) );
-        }
+        validateChildren(entity, sampleId);
+        validateDefaultAndFastLoad(entity, sampleId);
+    }
 
+    private void validateDefaultAndFastLoad(Entity entity, Long sampleId) throws Exception {
         // Checking on the fast-load file.
         Entity default3DImage = entity.getChildByAttributeName(EntityConstants.ATTRIBUTE_DEFAULT_3D_IMAGE);
         if ( default3DImage == null ) {
@@ -60,6 +71,34 @@ public class NeuronSeparatorPipelineResultValidator implements TypeValidator {
                 fileValidator.validateFile( fastLoadImageFile, EntityConstants.TYPE_MOVIE, sampleId, entity );
             }
             fileValidator.validateFile( default3dImageFile, EntityConstants.TYPE_IMAGE_3D, sampleId, entity );
+        }
+    }
+
+    private void validateChildren(Entity entity, Long sampleId) {
+        // Look for fragment collection.
+        Entity fragments = entity.getChildByAttributeName(EntityConstants.ATTRIBUTE_MASK_ENTITY_COLLECTION);
+        if ( fragments == null ) {
+            validationLogger.reportError(
+                    sampleId, entity.getId(), entity.getEntityTypeName(), NO_FRAGMENTS,
+                    String.format( ERROR_RPT_FMT, EntityConstants.ATTRIBUTE_MASK_ENTITY_COLLECTION, EntityConstants.TYPE_NEURON_SEPARATOR_PIPELINE_RESULT, entity.getId() ) );
+        }
+
+        // Look for specific attributes by name.
+        validateAttribute(entity, sampleId, EntityConstants.ATTRIBUTE_OBJECTIVE, NO_OBJECTIVE_ATTRIBUTE );
+        validateAttribute(entity, sampleId, EntityConstants.ATTRIBUTE_PIXEL_RESOLUTION, NO_PIXRES_ATTRIBUTE );
+        validateAttribute(entity, sampleId, EntityConstants.ATTRIBUTE_OPTICAL_RESOLUTION, NO_OPTICALRES_ATTRIBUTE );
+
+        // DEBUG: Check that something which should not be there, truly is not found, and hence validate validation.
+        // DEBUG:  validateAttribute(entity, sampleId, "Red Herring", FOOLS_ERRAND );
+
+    }
+
+    private void validateAttribute(Entity entity, Long sampleId, String attributeName, ValidationLogger.Category category) {
+        String value = entity.getValueByAttributeName( attributeName );
+        if ( value == null   ||   value.trim().length() == 0 ) {
+            validationLogger.reportError(
+                    sampleId, entity.getId(), entity.getEntityTypeName(), category,
+                    String.format( ERROR_RPT_FMT, attributeName, EntityConstants.TYPE_NEURON_SEPARATOR_PIPELINE_RESULT, entity.getId() ) );
         }
     }
 }
