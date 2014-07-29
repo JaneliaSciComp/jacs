12bf675b9798b3c0c7834ee42c36d6e5c2e8041f
Les Foster <fosterl@janalia.hhmi.org>
2014-07-29 17:10:45 -0400

    Saving migration to our very own pipeline queue.

---
+++ compute/src/org/janelia/it/jacs/compute/launcher/pipelines/ValidationPipelineLauncherMDB.java
@@ -0,0 +1,23 @@
+package org.janelia.it.jacs.compute.launcher.pipelines;
+
+import org.janelia.it.jacs.compute.engine.launcher.ejb.SeriesLauncherMDB;
+import org.jboss.annotation.ejb.PoolClass;
+import org.jboss.ejb3.StrictMaxPool;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+
+@MessageDriven(name = "ValidationtPipelineLauncherMDB", activationConfig = {
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge "),
+        @ActivationConfigProperty(propertyName = "messagingType", propertyValue = "javax.jms.MessageListener"),
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/validationPipelineLauncher"),
+        @ActivationConfigProperty(propertyName = "maxSession", propertyValue = "35"),
+        @ActivationConfigProperty(propertyName = "transactionTimeout", propertyValue = "432000"),
+        @ActivationConfigProperty(propertyName = "DLQMaxResent", propertyValue = "0")
+})
+@PoolClass(value = StrictMaxPool.class, maxSize = 20, timeout = 10000)
+
+@SuppressWarnings("unused")
+public class ValidationPipelineLauncherMDB extends SeriesLauncherMDB{
+}
