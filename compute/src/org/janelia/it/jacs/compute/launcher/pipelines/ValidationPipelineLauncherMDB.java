4510a1ad1a8c467d35a9b626a7672d7a4264a7ed
Les Foster <fosterl@janalia.hhmi.org>
2014-08-01 16:40:57 -0400

    Corrected mismatching annotation settings.

--- compute/src/org/janelia/it/jacs/compute/launcher/pipelines/ValidationPipelineLauncherMDB.java
+++ compute/src/org/janelia/it/jacs/compute/launcher/pipelines/ValidationPipelineLauncherMDB.java
@@ -7,7 +7,9 @@ import org.jboss.ejb3.StrictMaxPool;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 
-@MessageDriven(name = "ValidationtPipelineLauncherMDB", activationConfig = {
+// IMPORTANT: keep maxSize of StrictMaxPool in sync with maxSessions in activation config property.
+
+@MessageDriven(name = "ValidationPipelineLauncherMDB", activationConfig = {
         @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge "),
         @ActivationConfigProperty(propertyName = "messagingType", propertyValue = "javax.jms.MessageListener"),
         @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
@@ -16,7 +18,7 @@ import javax.ejb.MessageDriven;
         @ActivationConfigProperty(propertyName = "transactionTimeout", propertyValue = "432000"),
         @ActivationConfigProperty(propertyName = "DLQMaxResent", propertyValue = "0")
 })
-@PoolClass(value = StrictMaxPool.class, maxSize = 20, timeout = 10000)
+@PoolClass(value = StrictMaxPool.class, maxSize = 35, timeout = 10000)
 
 @SuppressWarnings("unused")
 public class ValidationPipelineLauncherMDB extends SeriesLauncherMDB{
