173528c0304a3b98b1ea102152d4e825c269aacc
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2014-08-01 15:03:25 -0400

    more options added to CreateTextSWCFile script

--- groovy/src/CreateTestSWCFile.groovy
+++ groovy/src/CreateTestSWCFile.groovy
@@ -17,12 +17,6 @@ defaultHeaderList << "# ORIGINAL_SOURCE CreateTestSWCFile.groovy"
 defaultHeaderList << "# OFFSET 0  0  0"
 
 
-nNodes = 10;
-originx = 0.0;
-originy = 0.0;
-originz = 0.0;
-stepsize = 100.0
-
 /**
  * returns a branch counter function that branches a constant number
  * of times
@@ -42,6 +36,48 @@ def constantStepAdder = {
 }
 
 /**
+ * returns a step adder that adds a step in a direction similar to the previous
+ * step (if there is no previous step, goes in +x direction)
+ *
+ * input angles are the half-width of the range from which the angle change
+ * is uniformly sampled (theta = polar, phi = azimuthal angle)
+ */
+def angleStepAdder = {
+    double dTheta, double dPhi, double stepsize, SWCNode parentNode, nodeList ->
+    if (parentNode.getParentIndex() == -1) {
+        dx = stepsize
+        dy = 0
+        dz = 0
+    } else {
+        x1 = parentNode.getX()
+        y1 = parentNode.getY()
+        z1 = parentNode.getZ()
+        grandparentNode = nodeList[parentNode.getParentIndex() - 1]
+        x2 = grandparentNode.getX()
+        y2 = grandparentNode.getY()
+        z2 = grandparentNode.getZ()
+
+        r = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) **2)
+        phi = Math.atan2(y1 - y2, x1 - x2)
+        theta = Math.acos((z1 - z2) / r)
+
+        phi += dPhi * 2 * (Math.random() - 0.5)
+        theta += dTheta * 2 * (Math.random() - 0.5)
+
+        sintheta = Math.sin(theta)
+        dx = sintheta * Math.cos(phi) * stepsize
+        dy = sintheta * Math.sin(phi) * stepsize
+        dz = Math.cos(theta) * stepsize
+    }
+    nodeList << new SWCNode(nodeList.size() + 1, 0,
+        parentNode.getX() + dx,
+        parentNode.getY() + dy,
+        parentNode.getZ() + dz,
+        1.0, parentNode.getIndex()
+    )
+}
+
+/**
  * given a nodelist and a step adder, add steps to a node list
  */
 def addBranch = {
@@ -52,9 +88,6 @@ def addBranch = {
     }
     return nodeList
 }
-// then:
-// myBranchAdder = addBranch.curry(nsteps, stepadder)
-
 
 /**
  * given a list of SWCNodes, return a list of those that have
@@ -89,26 +122,35 @@ def addGeneration(nodeList, branchCounter, branchAdder) {
 }
 
 /**
+ * create a new nodelist with one root node at the input location
+ */
+def createNeuronRoot = {
+    originx, originy, originz -> [new SWCNode(1, 0, originx, originy, originz, 1.0, -1)]
+}
+
+/**
  * create a neuron where each "generation" of branches uses the same
  * algorithms
  */
 def createSimpleNeuron = {
-    // probably ought to pass in the origin here, or maybe the start node?
-    branchCounter, branchAdder, nGenerations ->
-    nodeList = [new SWCNode(1, 0, 0.0, 0.0, 0.0, 1.0, -1)]
+    nodeList, branchCounter, branchAdder, nGenerations ->
     nGenerations.times {nodeList = addGeneration(nodeList, branchCounter, branchAdder)}
     return nodeList
 }
 
-/*
-// the obvious extension is different brancher and adder for each
-//  generation (pseudocode):
-createComplexNeuron(list (brancher, adder)) returns nodeList
-new nodeList = [root]
-for br, add in brancherAdderList:
-    nodeList = addGeneration(nodeList, br, add)
-return nodeList
+/**
+ * create a new neuron where each "generation" is built using
+ * a counter and adder from a list (one pair per generation)
  */
+ def createComplexNeuron = {
+    nodeList, counterAdderList ->
+    // annoyingly, you can't do "for x, y in list", as far as I can tell
+    for (pair in counterAdderList) {
+        nodeList = addGeneration(nodeList, *pair)
+    }
+    return nodeList
+ }
+
 
 
 // given all that, you create a neuron by combining low-level
@@ -117,7 +159,21 @@ return nodeList
 myBranchCounter = constantBranchCounter.curry(2)
 myStepAdder = constantStepAdder.curry(2.0, 3.0, 5.0)
 myBranchAdder = addBranch.curry(3, myStepAdder)
-nodeList = createSimpleNeuron(myBranchCounter, myBranchAdder, 2)
+// nodeList = createSimpleNeuron(createNeuronRoot(0.0, 0.0, 0.0), myBranchCounter, myBranchAdder, 2)
+
+
+// the numbers aren't great here, but it basically works
+nodeList = createComplexNeuron(
+    // these numbers correspond to the middle of sample 2014-02-27
+    createNeuronRoot(7000.0, 10000.0, 3200.0),
+    [
+        [constantBranchCounter.curry(1), addBranch.curry(3, constantStepAdder.curry(100.0, 0.0, 0.0))],
+        [constantBranchCounter.curry(2), addBranch.curry(1, angleStepAdder.curry(0.0, 0.5, 100.0))],
+        [constantBranchCounter.curry(1), addBranch.curry(5, constantStepAdder.curry(100.0, 0.0, 0.0))],
+        [constantBranchCounter.curry(2), addBranch.curry(1, angleStepAdder.curry(0.0, 0.5, 100.0))],
+        [constantBranchCounter.curry(1), addBranch.curry(5, constantStepAdder.curry(100.0, 0.0, 0.0))]
+    ])
+
 
 
 // create the SWC data object and write out the file
