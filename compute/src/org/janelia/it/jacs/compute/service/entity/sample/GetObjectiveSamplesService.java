32345b4ccdb228276da9d80cdbce0e1c732e2026
Konrad Rokicki <rokickik@janelia.hhmi.org>
2014-09-12 15:06:01 -0400

    decompress LSMs if necessary added ability to not rerun pipelines

--- compute/src/org/janelia/it/jacs/compute/service/entity/sample/GetObjectiveSamplesService.java
+++ compute/src/org/janelia/it/jacs/compute/service/entity/sample/GetObjectiveSamplesService.java
@@ -1,16 +1,24 @@
 package org.janelia.it.jacs.compute.service.entity.sample;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.janelia.it.jacs.compute.service.entity.AbstractEntityService;
 import org.janelia.it.jacs.model.entity.Entity;
 import org.janelia.it.jacs.model.entity.EntityConstants;
 import org.janelia.it.jacs.model.entity.cv.Objective;
+import org.janelia.it.jacs.model.tasks.Task;
 import org.janelia.it.jacs.shared.utils.EntityUtils;
-
 
 /**
  * Extracts sub-samples based on their objective.
+ * 
+ * If RERUN_PIPELINES is provided as false then some additional variable are processed:
+ * PIPELINES_TO_RUN_20X, PIPELINES_TO_RUN_40X. and PIPELINES_TO_RUN_63X
+ * 
+ * If a sub-sample already has results for all the pipelines given by its respective variable then its id is not output, 
+ * so that it is not run by subsequent processing.
  * 
  * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
  */
@@ -18,6 +26,26 @@ public class GetObjectiveSamplesService extends AbstractEntityService {
 
     public void execute() throws Exception {
 
+    	List<String> pipelines20x = null; 
+    	List<String> pipelines40x = null; 
+    	List<String> pipelines63x = null;
+    	boolean rerun = data.getItemAsBoolean("RERUN_PIPELINES");
+    	
+    	if (!rerun) {
+    		String pipelineStr20x = data.getItemAsString("PIPELINES_TO_RUN_20X");
+    		if (pipelineStr20x!=null) {
+    			pipelines20x = Task.listOfStringsFromCsvString(pipelineStr20x);
+    		}
+    		String pipelineStr40x = data.getItemAsString("PIPELINES_TO_RUN_40X");
+    		if (pipelineStr40x!=null) {
+    			pipelines40x = Task.listOfStringsFromCsvString(pipelineStr40x);
+    		}
+    		String pipelineStr63x = data.getItemAsString("PIPELINES_TO_RUN_63X");
+    		if (pipelineStr63x!=null) {
+    			pipelines63x = Task.listOfStringsFromCsvString(pipelineStr63x);
+    		}
+    	}
+    	
         final Entity sampleEntity = entityHelper.getRequiredSampleEntity(data);
         populateChildren(sampleEntity);
         final List<Entity> subSamples = EntityUtils.getChildrenOfType(sampleEntity, EntityConstants.TYPE_SAMPLE);
@@ -32,17 +60,25 @@ public class GetObjectiveSamplesService extends AbstractEntityService {
                 objective = subSample.getValueByAttributeName(EntityConstants.ATTRIBUTE_OBJECTIVE);
                 subSampleId = subSample.getId().toString();
                 if (Objective.OBJECTIVE_20X.getName().equals(objective)) {
-                    data.putItem("SAMPLE_20X_ID", subSampleId);
-                } else if (Objective.OBJECTIVE_40X.getName().equals(objective)) {
-                    data.putItem("SAMPLE_40X_ID", subSampleId);
-                } else if (Objective.OBJECTIVE_63X.getName().equals(objective)) {
-                    data.putItem("SAMPLE_63X_ID", subSampleId);
+                	if (rerun || !sampleHasAllPipelines(subSample, pipelines20x)) {
+                        data.putItem("SAMPLE_20X_ID", subSampleId);
+                	}
+                } 
+                else if (Objective.OBJECTIVE_40X.getName().equals(objective)) {
+                    if (rerun || !sampleHasAllPipelines(subSample, pipelines40x)) {
+                        data.putItem("SAMPLE_63X_ID", subSampleId);
+                	}
+                } 
+                else if (Objective.OBJECTIVE_63X.getName().equals(objective)) {
+                	if (rerun || !sampleHasAllPipelines(subSample, pipelines63x)) {
+                        data.putItem("SAMPLE_63X_ID", subSampleId);
+                	}
                 }
             }
 
-        } else {
-
-            contextLogger.info("No subSamples found");
+        } 
+        else {
+            contextLogger.info("No sub-samples found");
             String objective = sampleEntity.getValueByAttributeName(EntityConstants.ATTRIBUTE_OBJECTIVE);
             if (objective != null) {
                 data.putItem("SAMPLE_" + objective.toUpperCase() + "_ID", sampleEntity.getId().toString());
@@ -50,4 +86,32 @@ public class GetObjectiveSamplesService extends AbstractEntityService {
 
         }
     }
+
+	private boolean sampleHasAllPipelines(Entity subSample, List<String> pipelines) throws Exception {
+
+		if (pipelines==null) return false;
+    	populateChildren(subSample);
+		
+    	Set<String> pipelineSet = new HashSet<String>(pipelines);
+    	
+        for(Entity pipelineRun : EntityUtils.getChildrenOfType(subSample, EntityConstants.TYPE_PIPELINE_RUN)) {
+        	populateChildren(subSample);
+        	if (EntityUtils.findChildWithType(subSample, EntityConstants.TYPE_ERROR)!=null) {
+        		continue;
+        	}
+        	String pipelineName = pipelineRun.getValueByAttributeName(EntityConstants.ATTRIBUTE_PIPELINE_PROCESS);
+        	if (pipelineName!=null) {
+        		pipelineSet.remove(pipelineName);
+        	}
+        }
+    	
+        if (pipelineSet.isEmpty()) {
+        	logger.info("Sample "+subSample.getName()+" has no unfulfilled pipelines");
+        }
+        else {
+        	logger.info("Sample "+subSample.getName()+" has unfulfilled pipelines: "+pipelineSet);	
+        }
+        
+		return pipelineSet.isEmpty();
+	}
 }
