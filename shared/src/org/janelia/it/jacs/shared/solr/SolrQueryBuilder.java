48c49ee059edaddcf7f417f730a9ec5dc67101f8
Konrad Rokicki <rokickik@janelia.hhmi.org>
2015-03-30 15:45:55 -0400

    fixed general search by date and date range

--- shared/src/org/janelia/it/jacs/shared/solr/SolrQueryBuilder.java
+++ shared/src/org/janelia/it/jacs/shared/solr/SolrQueryBuilder.java
@@ -2,51 +2,47 @@ package org.janelia.it.jacs.shared.solr;
 
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrQuery.ORDER;
-import org.janelia.it.jacs.shared.solr.SolrDocTypeEnum;
-import org.janelia.it.jacs.shared.solr.SolrUtils;
 import org.janelia.it.jacs.shared.utils.StringUtils;
 
 import java.util.*;
 
 /**
  * A helper class for building SOLR queries against the Entity model.
- * 
+ *
  * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
  */
 public class SolrQueryBuilder {
 
-	private String searchString;
-	private String auxString;
-	private String auxAnnotationQueryString;
-	private Long rootId;
-	private List<String> ownerKeys = new ArrayList<String>();
-	private Map<String,Set<String>> filters = new HashMap<String,Set<String>>();
-	private List<String> facets = new ArrayList<String>();
-	private String sortField;
-	private boolean ascending;
-	private Date startDate;
-	private Date endDate;
-		
-	public SolrQueryBuilder() {
-	}
-	
-	public String getSearchString() {
-		return searchString;
-	}
+    private String searchString;
+    private String auxString;
+    private String auxAnnotationQueryString;
+    private Long rootId;
+    private List<String> ownerKeys = new ArrayList<>();
+    private Map<String, Set<String>> filters = new HashMap<>();
+    private List<String> facets = new ArrayList<>();
+    private String sortField;
+    private boolean ascending;
 
-	public void setSearchString(String searchString) {
-		this.searchString = searchString;
-	}
+    public SolrQueryBuilder() {
+    }
 
-	public String getAuxString() {
-		return auxString;
-	}
+    public String getSearchString() {
+        return searchString;
+    }
 
-	public void setAuxString(String auxString) {
-		this.auxString = auxString;
-	}
+    public void setSearchString(String searchString) {
+        this.searchString = searchString;
+    }
 
-	public String getAuxAnnotationQueryString() {
+    public String getAuxString() {
+        return auxString;
+    }
+
+    public void setAuxString(String auxString) {
+        this.auxString = auxString;
+    }
+
+    public String getAuxAnnotationQueryString() {
         return auxAnnotationQueryString;
     }
 
@@ -55,169 +51,157 @@ public class SolrQueryBuilder {
     }
 
     public Long getRootId() {
-		return rootId;
-	}
+        return rootId;
+    }
 
-	public void setRootId(Long rootId) {
-		this.rootId = rootId;
-	}
+    public void setRootId(Long rootId) {
+        this.rootId = rootId;
+    }
 
-	public List<String> getOwnerKeys() {
-		return ownerKeys;
-	}
+    public List<String> getOwnerKeys() {
+        return ownerKeys;
+    }
 
-	public void addOwnerKey(String ownerKey) {
-		this.ownerKeys.add(ownerKey);
-	}
+    public void addOwnerKey(String ownerKey) {
+        this.ownerKeys.add(ownerKey);
+    }
 
-	public Map<String, Set<String>> getFilters() {
-		return filters;
-	}
+    public Map<String, Set<String>> getFilters() {
+        return filters;
+    }
 
-	public void setFilters(Map<String, Set<String>> filters) {
-		this.filters = filters;
-	}
+    public void setFilters(Map<String, Set<String>> filters) {
+        this.filters = filters;
+    }
 
-	public List<String> getFacets() {
-		return facets;
-	}
+    public List<String> getFacets() {
+        return facets;
+    }
 
-	public void setFacets(List<String> facets) {
-		this.facets = facets;
-	}
+    public void setFacets(List<String> facets) {
+        this.facets = facets;
+    }
 
-	public String getSortField() {
-		return sortField;
-	}
+    public String getSortField() {
+        return sortField;
+    }
 
-	public void setSortField(String sortField) {
-		this.sortField = sortField;
-	}
+    public void setSortField(String sortField) {
+        this.sortField = sortField;
+    }
 
-	public boolean isAscending() {
-		return ascending;
-	}
+    public boolean isAscending() {
+        return ascending;
+    }
 
-	public void setAscending(boolean ascending) {
-		this.ascending = ascending;
-	}
+    public void setAscending(boolean ascending) {
+        this.ascending = ascending;
+    }
 
-	public Date getStartDate() {
-		return startDate;
-	}
+    public boolean hasQuery() {
+        return !StringUtils.isEmpty(searchString) || !StringUtils.isEmpty(auxString) || !StringUtils.isEmpty(auxAnnotationQueryString) || rootId != null || !filters.isEmpty();
+    }
 
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
+    public SolrQuery getQuery() throws Exception {
 
-	public Date getEndDate() {
-		return endDate;
-	}
+        StringBuilder qs = new StringBuilder();
 
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
-	
-	public boolean hasQuery() {
-		return !StringUtils.isEmpty(searchString) || !StringUtils.isEmpty(auxString) || !StringUtils.isEmpty(auxAnnotationQueryString) || rootId!=null || !filters.isEmpty() || startDate!=null || endDate != null;
-	}
-	
-	public SolrQuery getQuery() throws Exception {
-    	
-    	StringBuffer qs = new StringBuffer();
-    	
-    	if (!ownerKeys.isEmpty()) {
-        	qs.append("+subjects:(");
-        	int i = 0;
-        	for(String ownerKey : ownerKeys) {
-        		if (i++>0) qs.append(" OR ");
+        if (!ownerKeys.isEmpty()) {
+            qs.append("+subjects:(");
+            int i = 0;
+            for (String ownerKey : ownerKeys) {
+                if (i++ > 0) {
+                    qs.append(" OR ");
+                }
                 String ownerName = ownerKey.split(":")[1];
-        		qs.append(ownerName);
-        	}
-        	qs.append(")");
-    	}
-    	
-    	if (rootId != null) {
-    		qs.append(" AND (ancestor_ids:").append(rootId).append(")");
-    	}
-    	
-    	qs.append(" +doc_type:").append(SolrDocTypeEnum.ENTITY.toString());
-    	
-    	qs.append(" -entity_type:Ontology*");
+                qs.append(ownerName);
+            }
+            qs.append(")");
+        }
 
-		if (startDate!=null || endDate!=null) {
-			String startDateStr = startDate==null ? "*" : SolrUtils.formatDate(startDate);
-			String endDateStr = endDate==null ? "*" : SolrUtils.formatDate(endDate);
-			qs.append(" +updated_date:[").append(startDateStr).append(" TO ").append(endDateStr).append("]");
-		}
+        if (rootId != null) {
+            qs.append(" AND (ancestor_ids:").append(rootId).append(")");
+        }
 
-		if (!StringUtils.isEmpty(auxString)) {
-			qs.append(" +(");
-			qs.append(auxString);
-			qs.append(")");
-		}
+        qs.append(" +doc_type:").append(SolrDocTypeEnum.ENTITY.toString());
 
-		boolean outer = !StringUtils.isEmpty(searchString) || !StringUtils.isEmpty(auxAnnotationQueryString);
-        if (outer) qs.append(" +(");
-        
+        qs.append(" -entity_type:Ontology*");
+
+        if (!StringUtils.isEmpty(auxString)) {
+            qs.append(" +(");
+            qs.append(auxString);
+            qs.append(")");
+        }
+
+        boolean outer = !StringUtils.isEmpty(searchString) || !StringUtils.isEmpty(auxAnnotationQueryString);
+        if (outer) {
+            qs.append(" +(");
+        }
+
         if (!StringUtils.isEmpty(searchString)) {
-            String escapedSearchString = searchString==null?"":searchString.replaceAll(":", "\\:");
+            String escapedSearchString = searchString == null ? "" : searchString.replaceAll(":", "\\:");
             qs.append("(").append(escapedSearchString).append(")");
             if (StringUtils.isEmpty(auxAnnotationQueryString)) {
-                qs.append(" OR ");   
+                qs.append(" OR ");
             }
             else {
                 qs.append(" AND ");
             }
-        }      
+        }
 
         String annotationSearchString = getAnnotationSearchString();
         if (!StringUtils.isEmpty(annotationSearchString)) {
             qs.append("(");
             int i = 0;
-        	for(String ownerKey : ownerKeys) {
+            for (String ownerKey : ownerKeys) {
                 String ownerName = ownerKey.split(":")[1];
-        		String fieldNamePrefix = SolrUtils.getFormattedName(ownerName);
-        		if (i++>0) {
-        		    qs.append(" OR ");
-        		}
-        		qs.append(fieldNamePrefix).append("_annotations:").append(annotationSearchString);	
-        	}
+                String fieldNamePrefix = SolrUtils.getFormattedName(ownerName);
+                if (i++ > 0) {
+                    qs.append(" OR ");
+                }
+                qs.append(fieldNamePrefix).append("_annotations:").append(annotationSearchString);
+            }
             qs.append(")");
         }
-        
-        if (outer) qs.append(")");
-		
-    	SolrQuery query = new SolrQuery();
+
+        if (outer) {
+            qs.append(")");
+        }
+
+        SolrQuery query = new SolrQuery();
         query.setQuery(qs.toString());
         query.setFacetMinCount(1);
-		query.addField("score");
+        query.addField("score");
 
-		if (sortField!=null) {
-			query.setSortField(sortField, ascending?ORDER.asc:ORDER.desc);
-		}
+        if (sortField != null) {
+            query.setSortField(sortField, ascending ? ORDER.asc : ORDER.desc);
+        }
 
-		for(String fieldName : filters.keySet()) {
-			Set<String> values = filters.get(fieldName);
-			if (values==null||values.isEmpty()) continue;
-			query.addFilterQuery(getFilterQuery(fieldName, values));
-		}
-		
-		for(String facet : facets) {
-			// Exclude the facet field from itself, to support multi-valued faceting
-    		query.addFacetField("{!ex="+facet+"}"+facet);
-		}
-		
+        for (String fieldName : filters.keySet()) {
+            Set<String> values = filters.get(fieldName);
+            if (values == null || values.isEmpty()) {
+                continue;
+            }
+            query.addFilterQuery(getFilterQuery(fieldName, values));
+        }
+
+        for (String facet : facets) {
+            // Exclude the facet field from itself, to support multi-valued faceting
+            query.addFacetField("{!ex=" + facet + "}" + facet);
+        }
+
         return query;
     }
-	
-	private String getAnnotationSearchString() {
-	    StringBuilder query = new StringBuilder();
-	    String escapedSearchString = null;
-	    if (StringUtils.isEmpty(auxAnnotationQueryString) && !StringUtils.isEmpty(searchString)) {
-            escapedSearchString = searchString.replaceAll(":", "\\:");
-	    }
-	    else {
+
+    private String getAnnotationSearchString() {
+        StringBuilder query = new StringBuilder();
+        String escapedSearchString = null;
+        if (StringUtils.isEmpty(auxAnnotationQueryString)) {
+            if (!StringUtils.isEmpty(searchString)) {
+                escapedSearchString = searchString.replaceAll(":", "\\:");
+            }
+        }
+        else  {
             escapedSearchString = auxAnnotationQueryString.replaceAll(":", "\\:");
         }
         if (!StringUtils.isEmpty(escapedSearchString)) {
@@ -225,26 +209,27 @@ public class SolrQueryBuilder {
             query.append(escapedSearchString);
             query.append(")");
         }
-	    return query.toString();
-	}
+        return query.toString();
+    }
 
     /**
      * Returns a SOLR-style field query for the given field containing the given values. Also tags the
-     * field so that it can be excluded in facets on other fields. 
+     * field so that it can be excluded in facets on other fields.
+     *
      * @param fieldName
      * @param values
      * @return
      */
-	protected String getFilterQuery(String fieldName, Set<String> values) {
-    	StringBuffer sb = new StringBuffer();
-    	sb.append("{!tag=").append(fieldName).append("}").append(fieldName);
-    	sb.append(":("); // Sad face :/
-    	for(String value : values) {
-    		sb.append("\"");
-    		sb.append(value);
-    		sb.append("\" ");
-    	}
-    	sb.append(")");
-    	return sb.toString();
+    protected String getFilterQuery(String fieldName, Set<String> values) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{!tag=").append(fieldName).append("}").append(fieldName);
+        sb.append(":("); // Sad face :/
+        for (String value : values) {
+            sb.append("\"");
+            sb.append(value);
+            sb.append("\" ");
+        }
+        sb.append(")");
+        return sb.toString();
     }
 }
--- shared/src/org/janelia/it/jacs/shared/solr/SolrUtils.java
+++ shared/src/org/janelia/it/jacs/shared/solr/SolrUtils.java
@@ -1,6 +1,5 @@
 package org.janelia.it.jacs.shared.solr;
 
-
 import org.janelia.it.jacs.shared.utils.StringUtils;
 
 import java.text.SimpleDateFormat;
@@ -8,74 +7,92 @@ import java.util.Date;
 
 /**
  * Utilities used by the server, which are likewise useful in the client.
- * 
+ *
  * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
  */
 public class SolrUtils {
 
-	
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T00:00:00Z'");
+    private static final SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
     /**
-     * Format the given date according to "Complete ISO 8601 Date" format supported by SOLR query syntax.
+     * Format the given date according to the year/month/day part of the "Complete ISO 8601 Date" format supported by SOLR query syntax.
+     *
      * @param date
      * @return
      */
     public static String formatDate(Date date) {
-    	return dateFormat.format(date);
+        return dateFormat.format(date);
     }
 
-	/**
-	 * Format the given name in lowercase, with underscores instead of spaces. For example, "Tiling Pattern" -> "tiling_pattern"
-	 * @param name
-	 * @return
-	 */
+    /**
+     * Format the given date according to "Complete ISO 8601 Date" format supported by SOLR query syntax.
+     *
+     * @param date
+     * @return
+     */
+    public static String formatDateTime(Date date) {
+        return dateTimeFormat.format(date);
+    }
+    
+    /**
+     * Format the given name in lowercase, with underscores instead of spaces. For example, "Tiling Pattern" -> "tiling_pattern"
+     *
+     * @param name
+     * @return
+     */
     public static String getFormattedName(String name) {
-    	return name.toLowerCase().replaceAll("\\W+", "_");
+        return name.toLowerCase().replaceAll("\\W+", "_");
     }
 
-	/**
-	 * Get the SOLR field name from an attribute name. For example, "Tiling Pattern" -> "tiling_pattern_txt"
-	 * @param name
-	 * @return
-	 */
-    public static String getDynamicFieldName(String name) {
-    	return getFormattedName(name)+"_txt";
-    }
     /**
      * Get the SOLR field name from an attribute name. For example, "Tiling Pattern" -> "tiling_pattern_txt"
+     *
+     * @param name
+     * @return
+     */
+    public static String getDynamicFieldName(String name) {
+        return getFormattedName(name) + "_txt";
+    }
+
+    /**
+     * Get the SOLR field name from an attribute name. For example, "Tiling Pattern" -> "tiling_pattern_txt"
+     *
      * @param name
      * @return
      */
     public static String getAttributeNameFromSolrFieldName(String solrFieldName) {
-        if (solrFieldName==null) return null;
+        if (solrFieldName == null) {
+            return null;
+        }
         String name = solrFieldName.replaceFirst("_txt", "");
         return StringUtils.underscoreToTitleCase(name);
     }
-    
+
     /**
      * Get the SOLR field name for a Sage CV term. For example, "effector" -> "sage_effector_t"
+     *
      * @param term
      * @param sageType a data type from Sage, such as "float" or "integer" or "text" or "date_time"
      * @return
      */
     public static String getSageFieldName(SageTerm sageTerm) {
-    	String sageType = sageTerm.getDataType();
-		String solrSuffix = "_t"; // default to text-based indexing
-		if (sageType != null) {
-			if ("float".equals(sageType)) {
-				solrSuffix = "_d";
-			}
-			else if ("date".equals(sageType) || "date_time".equals(sageType)) {
-				solrSuffix = "_dt";
-			}
-			else if ("boolean".equals(sageType)) {
-				solrSuffix = "_b";
-			}
-			else if ("integer".equals(sageType)) {
-				solrSuffix = "_l";		
-			}
-		}
-		return "sage_"+sageTerm.getKey()+solrSuffix;
+        String sageType = sageTerm.getDataType();
+        String solrSuffix = "_t"; // default to text-based indexing
+        if (sageType != null) {
+            if ("float".equals(sageType)) {
+                solrSuffix = "_d";
+            }
+            else if ("date".equals(sageType) || "date_time".equals(sageType)) {
+                solrSuffix = "_dt";
+            }
+            else if ("boolean".equals(sageType)) {
+                solrSuffix = "_b";
+            }
+            else if ("integer".equals(sageType)) {
+                solrSuffix = "_l";
+            }
+        }
+        return "sage_" + sageTerm.getKey() + solrSuffix;
     }
 }
