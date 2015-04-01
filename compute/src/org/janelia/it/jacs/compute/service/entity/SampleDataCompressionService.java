53d2b9ef587ce82fe5c15efe3d5b32e71586a65f
Konrad Rokicki <rokickik@janelia.hhmi.org>
2015-04-01 17:15:52 -0400

    implemented h5j compression pipeline

--- compute/src/org/janelia/it/jacs/compute/service/entity/SampleDataCompressionService.java
+++ compute/src/org/janelia/it/jacs/compute/service/entity/SampleDataCompressionService.java
@@ -1,32 +1,35 @@
 package org.janelia.it.jacs.compute.service.entity;
 
-import org.apache.log4j.Logger;
-import org.janelia.it.jacs.compute.api.*;
-import org.janelia.it.jacs.compute.engine.data.IProcessData;
-import org.janelia.it.jacs.compute.engine.service.IService;
-import org.janelia.it.jacs.compute.engine.service.ServiceException;
-import org.janelia.it.jacs.compute.service.common.ProcessDataHelper;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.janelia.it.jacs.compute.api.ComputeException;
+import org.janelia.it.jacs.compute.api.EJBFactory;
 import org.janelia.it.jacs.compute.util.FileUtils;
 import org.janelia.it.jacs.model.common.SystemConfigurationProperties;
 import org.janelia.it.jacs.model.entity.Entity;
 import org.janelia.it.jacs.model.entity.EntityConstants;
-import org.janelia.it.jacs.model.tasks.Task;
-import org.janelia.it.jacs.model.user_data.Subject;
 import org.janelia.it.jacs.shared.utils.EntityUtils;
 
-import java.io.File;
-import java.util.*;
-
 /**
- * Compress existing v3draw files to v3dpbd to save space.
+ * Compress a set of existing files to a new set of formats.  
  *   
  * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
  */
-public class SampleDataCompressionService implements IService {
+public class SampleDataCompressionService extends AbstractEntityService {
 
 	public transient static final String CENTRAL_DIR_PROP = "FileStore.CentralDir";
 	
 	private static final String centralDir = SystemConfigurationProperties.getString(CENTRAL_DIR_PROP);
+	
+	public static final String RECORD_MODE_UPDATE = "UPDATE";
+    public static final String RECORD_MODE_ADD = "ADD";
 	
     public static final String MODE_UNDEFINED = "UNDEFINED";
     public static final String MODE_CREATE_INPUT_LIST = "CREATE_INPUT_LIST";
@@ -35,71 +38,47 @@ public class SampleDataCompressionService implements IService {
     public static final int GROUP_SIZE = 200;
     public transient static final String PARAM_testRun = "is test run";
 	
-    protected Logger logger;
-    protected Task task;
-    protected String ownerKey;
-    protected AnnotationBeanLocal annotationBean;
-    protected EntityBeanLocal entityBean;
-    protected ComputeBeanLocal computeBean;
-    
-    protected int numChanges;
-    
     private boolean isDebug = false;
-	protected IProcessData processData;
+
+    private String mode = MODE_UNDEFINED;
+    private String recordMode = RECORD_MODE_UPDATE;
+    private boolean deleteSourceFiles = true;
     
     private String rootEntityId;
-    private Set<String> inputFiles = new HashSet<>();
-    private Map<String,Set<Long>> entityMap = new HashMap<>();
-    
-    public void execute(IProcessData processData) throws ServiceException {
+    private Set<String> inputFiles = new HashSet<String>();
+    private Map<String,Set<Long>> entityMap = new HashMap<String,Set<Long>>();
 
-    	try {
-            logger = ProcessDataHelper.getLoggerForTask(processData, this.getClass());
-            task = ProcessDataHelper.getTask(processData);
-            annotationBean = EJBFactory.getLocalAnnotationBean();
-            entityBean = EJBFactory.getLocalEntityBean();
-            computeBean = EJBFactory.getLocalComputeBean();
-            
-            String ownerName = ProcessDataHelper.getTask(processData).getOwner();
-            Subject subject = computeBean.getSubjectByNameOrKey(ownerName);
-            this.ownerKey = subject.getKey();
-            
-            String testRun = task.getParameter(PARAM_testRun);
-            if (testRun!=null) {
-            	isDebug = Boolean.parseBoolean(testRun);	
-            }
+    protected int numChanges;
 
-			String mode = processData.getString("MODE");
-            this.processData = processData;
+    public void execute() throws Exception {
 
-        	rootEntityId = (String)processData.getItem("ROOT_ENTITY_ID");
-
-			switch (mode) {
-				case MODE_CREATE_INPUT_LIST:
-					doCreateInputList();
-					break;
-				case MODE_CREATE_OUTPUT_LIST:
-					doCreateOutputList();
-					break;
-				case MODE_COMPLETE:
-					doComplete();
-					break;
-				default:
-					logger.error("Do not recognize mode type=" + mode);
-					break;
-			}
-    	}
-        catch (Exception e) {
-        	if (e instanceof ServiceException) {
-            	throw (ServiceException)e;
-            }
-            throw new ServiceException("Error running SampleDataCompressionService", e);
+        String testRun = task.getParameter(PARAM_testRun);
+        if (testRun!=null) {
+        	isDebug = Boolean.parseBoolean(testRun);	
+        }
+        
+        mode = data.getRequiredItemAsString("MODE");
+    	rootEntityId = data.getItemAsString("ROOT_ENTITY_ID");
+    	
+        if (mode.equals(MODE_CREATE_INPUT_LIST)) {
+            doCreateInputList();
+        }
+        else if (mode.equals(MODE_CREATE_OUTPUT_LIST)) {
+            doCreateOutputList();
+        }
+        else if (mode.equals(MODE_COMPLETE)) {
+            doComplete();
+        } 
+        else {
+            throw new IllegalStateException("Do not recognize mode '"+mode+"'");
         }
     }
     
     private void doCreateInputList() throws ComputeException {
 
-        processV3dRawEntities();
+        String inputType = data.getRequiredItemAsString("INPUT_TYPE");
+        
+        processEntities(inputType);
         
         List<List<String>> inputGroups = createGroups(inputFiles, GROUP_SIZE);
         processData.putItem("INPUT_PATH_LIST", inputGroups);
@@ -134,19 +113,19 @@ public class SampleDataCompressionService implements IService {
     
     private void doCreateOutputList() throws ComputeException {
 
-    	List<String> inputPaths = (List<String>)processData.getItem("INPUT_PATH_LIST");
-    	if (inputPaths == null) {
-    		throw new IllegalArgumentException("INPUT_PATH_LIST may not be null");
-    	}
-    	
-    	List<String> outputPaths = new ArrayList<>();
-    	for(String s : inputPaths) {
-    		outputPaths.add(s.replaceAll("v3draw", "v3dpbd"));
+
+        String outputType = data.getRequiredItemAsString("OUTPUT_TYPE");
+    	List<String> inputPaths = (List<String>)data.getRequiredItem("INPUT_PATH_LIST");
+
+        List<String> outputPaths = new ArrayList<String>();
+    	for(String filepath : inputPaths) {
+    	    String extension = getExtension(filepath);
+            outputPaths.add(filepath.replaceAll(extension, outputType));
     	}
         processData.putItem("OUTPUT_PATH_LIST", outputPaths);
     }
     
-	public void processV3dRawEntities() throws ComputeException {
+	public void processEntities(String extension) throws ComputeException {
 
         if (isDebug) {
             logger.info("This is a test run. Nothing will actually happen.");
@@ -156,7 +135,7 @@ public class SampleDataCompressionService implements IService {
         }
         
         if (rootEntityId!=null) {
-            logger.info("Finding V3DRAW files to compress under root "+rootEntityId);
+            logger.info("Finding files to compress under root "+rootEntityId+" with type "+extension);
         	
         	Entity entity = EJBFactory.getLocalEntityBean().getEntityTree(new Long(rootEntityId));
         	if (entity == null) {
@@ -165,29 +144,29 @@ public class SampleDataCompressionService implements IService {
         	
         	for(Entity image : EntityUtils.getDescendantsOfType(entity,EntityConstants.TYPE_IMAGE_3D)) {
         		String filepath = image.getValueByAttributeName(EntityConstants.ATTRIBUTE_FILE_PATH);
-        		if (filepath!=null && filepath.endsWith("v3draw")) {
-        			addV3dRawFile(image);
+        		if (filepath!=null && filepath.endsWith(extension)) {
+        			addEntityToInputList(image);
         		}
         	}
         }
         else {
-        	logger.info("Finding V3DRAW files belonging to "+ownerKey);
+        	logger.info("Finding files belonging to "+ownerKey+" with type "+extension);
         	
-    		for(Entity entity : entityBean.getUserEntitiesWithAttributeValue(ownerKey, EntityConstants.ATTRIBUTE_FILE_PATH, "%v3draw")) {
+    	    for(Entity entity : entityBean.getUserEntitiesWithAttributeValue(ownerKey, EntityConstants.ATTRIBUTE_FILE_PATH, "%"+extension)) {
     			if (!entity.getEntityTypeName().equals(EntityConstants.TYPE_IMAGE_3D)) {
-    				logger.warn("Ignoring entity with v3draw filepath that is not an Image 3D: "+entity.getId());
+    				logger.warn("Ignoring entity with filepath that is not an Image 3D: "+entity.getId());
     				continue;
     			}
     			if (!entity.getValueByAttributeName(EntityConstants.ATTRIBUTE_FILE_PATH).contains(centralDir)) {
     				logger.warn("Ignoring entity with which does not contain the FileStore.CentralDir: "+entity.getId());
     				continue;
     			}
-    			addV3dRawFile(entity);
+    			addEntityToInputList(entity);
     		}
         }
     }
     
-    private void addV3dRawFile(Entity imageEntity) throws ComputeException {
+    private void addEntityToInputList(Entity imageEntity) throws ComputeException {
 
     	if (!imageEntity.getOwnerKey().equals(ownerKey)) return;
     	
@@ -219,87 +198,129 @@ public class SampleDataCompressionService implements IService {
     
     private void doComplete() throws ComputeException {
 
-    	this.entityMap = (Map<String,Set<Long>>)processData.getItem("ENTITY_MAP");
-    	List<String> inputPaths = (List<String>)processData.getItem("INPUT_PATH_LIST");
-    	if (inputPaths == null) {
-    		throw new IllegalArgumentException("INPUT_PATH_LIST may not be null");
-    	}
-    	
-    	List<String> outputPaths = (List<String>)processData.getItem("OUTPUT_PATH_LIST");
-    	if (outputPaths == null) {
-    		throw new IllegalArgumentException("OUTPUT_PATH_LIST may not be null");
-    	}
+        this.recordMode = data.getRequiredItemAsString("RECORD_MODE");
+        this.deleteSourceFiles = !"false".equals(data.getRequiredItem("DELETE_INPUTS"));
+    	this.entityMap = (Map<String,Set<Long>>)data.getRequiredItem("ENTITY_MAP");
+    	List<String> inputPaths = (List<String>)data.getRequiredItem("INPUT_PATH_LIST");
+        List<String> outputPaths = (List<String>)data.getRequiredItem("OUTPUT_PATH_LIST");
     	
     	for(int i=0; i<inputPaths.size(); i++) {
     		String inputPath = inputPaths.get(i);
-    		String outputPath = outputPaths.get(i);
-    		File outputFile = new File(outputPath);
-    		if (!outputFile.exists() || !outputFile.canRead() || outputFile.length()<=0) {
-    			logger.warn("Missing or corrupt output file: "+outputFile);
-    		}
-    		else {
-    			updateEntities(inputPath, outputPath);	
-    		}
+            String outputPath = outputPaths.get(i);
+            File outputFile = new File(outputPath);
+            if (!outputFile.exists() || !outputFile.canRead() || outputFile.length()<=0) {
+                logger.warn("Missing or corrupt output file: "+outputFile);
+            }
+            else {
+                updateEntities(inputPath, outputPath);  
+            }
     	}
 
 		logger.info("Modified "+numChanges+" entities.");
     }
     
     private void updateEntities(String inputPath, String outputPath) throws ComputeException {
-
+        
+        String inputExtension = getExtension(inputPath);
+        String outputExtension = getExtension(outputPath);
+        
     	try {
-    	    // Update all entities which referred to the old path
-    	    int numUpdated = entityBean.bulkUpdateEntityDataValue(inputPath, outputPath);
-            logger.info("Updated "+numUpdated+" entities to use new compressed file: "+outputPath);
-
+            File outputFile = new File(outputPath);
+            if (!outputFile.exists() || !outputFile.canRead() || outputFile.length()<=0) {
+                logger.warn("Missing or corrupt output file: "+outputFile);
+                return;
+            }
+            
+            if (RECORD_MODE_UPDATE.equals(recordMode)) {
+                if (!isDebug) {
+            	    // Update all entities which referred to the old path
+            	    int numUpdated = entityBean.bulkUpdateEntityDataValue(inputPath, outputPath);
+                    logger.info("Updated "+numUpdated+" entities to use new compressed file: "+outputPath);
+                }
+            }
+            
             Set<Long> inputEntites = entityMap.get(inputPath);
             if (inputEntites == null) {
                 logger.warn("No entities found with this path: "+inputPath);
                 return;
             }
             
+            Entity secEntity = null;
+            
     	    // Update the input stacks
-        	for(Entity entity : entityBean.getEntitiesById(new ArrayList<>(inputEntites))) {
-        	    
-        	    // Update the path. This was already fixed by the bulk update above, but we need to fix it again, 
-        	    // because we're using an entity with the old state.
-                entity.setValueByAttributeName(EntityConstants.ATTRIBUTE_FILE_PATH, outputPath);
+
+        	for(Entity entity : entityBean.getEntitiesById(new ArrayList<Long>(inputEntites))) {
+
+                if (RECORD_MODE_UPDATE.equals(recordMode)) {
+                    // Update the format
+            		String format = entity.getValueByAttributeName(EntityConstants.ATTRIBUTE_IMAGE_FORMAT);
+            		if (format != null && !"".equals(format)) {
+            			entity.setValueByAttributeName(EntityConstants.ATTRIBUTE_IMAGE_FORMAT, outputExtension);
+            		}
+            		
+            		// Update the name
+            		if (entity.getName().endsWith(inputExtension)) {
+            			entity.setName(entity.getName().replaceAll(inputExtension, outputExtension));
+            		}
+            		
+            		if (!isDebug) {
+    	            	Entity savedEntity = entityBean.saveOrUpdateEntity(entity);
+    	            	logger.info("Updated entity: "+savedEntity.getName()+" (id="+savedEntity.getId()+")");
+            		}
+            		else {
+            			logger.info("Updated entity: "+entity.getName()+" (id="+entity.getId()+")");
+            		}
+                }
+                else if (RECORD_MODE_ADD.equals(recordMode)) {
+                    if (secEntity==null) {
+                        if (!isDebug) {
+                            String secName = entity.getName().replaceAll(inputExtension, outputExtension);
+                            secEntity = entityBean.createEntity(entity.getOwnerKey(), EntityConstants.TYPE_IMAGE_3D, secName);
+    
+                            // Update the format
+                            String format = entity.getValueByAttributeName(EntityConstants.ATTRIBUTE_IMAGE_FORMAT);
+                            if (format != null && !"".equals(format)) {
+                                entity.setValueByAttributeName(EntityConstants.ATTRIBUTE_IMAGE_FORMAT, outputExtension);
+                            }
+                            entityBean.saveOrUpdateEntity(entity);
+                        }
+                        logger.info("Created secondary entity: "+secEntity.getName()+" (id="+secEntity.getId()+")");
+                    }
+                    if (!isDebug) {
+                        entityBean.addEntityToParent(entity, secEntity, entity.getMaxOrderIndex()+1, EntityConstants.ATTRIBUTE_LOSSY_IMAGE);
+                    }
+                }
+                else {
+                    throw new IllegalStateException("Illegal RECORD_MODE: "+recordMode);
+                }
                 
-                // Update the format
-        		String format = entity.getValueByAttributeName(EntityConstants.ATTRIBUTE_IMAGE_FORMAT);
-        		if (format != null && !"".equals(format)) {
-        			entity.setValueByAttributeName(EntityConstants.ATTRIBUTE_IMAGE_FORMAT, "v3dpbd");
-        		}
-        		
-        		// Update the name
-        		if (entity.getName().endsWith(".v3draw")) {
-        			entity.setName(entity.getName().replaceAll("v3draw", "v3dpbd"));
-        		}
-        		
-        		if (!isDebug) {
-	            	Entity savedEntity = entityBean.saveOrUpdateEntity(entity);
-	            	logger.info("Updated entity: "+savedEntity.getName()+" (id="+savedEntity.getId()+")");
-        		}
-        		else {
-        			logger.info("Updated entity: "+entity.getName()+" (id="+entity.getId()+")");
-        		}
             	numChanges++;
-        	}	
+        	}
     	
-    		File file = new File(inputPath);
-    		if (!isDebug) {
-				try {
-					FileUtils.forceDelete(file);
-					logger.info("Deleted old file: "+inputPath);
-				}
-				catch (Exception e) {
-					logger.info("Error deleting symlink "+inputPath+": "+e.getMessage());
-				}
-    		}
+        	if (deleteSourceFiles) {
+        		File file = new File(inputPath);
+        		if (!isDebug) {
+    				try {
+    					FileUtils.forceDelete(file);
+    					logger.info("Deleted old file: "+inputPath);
+    				}
+    				catch (Exception e) {
+    					logger.info("Error deleting symlink "+inputPath+": "+e.getMessage());
+    				}
+        		}
+        	}
     		
     	}
     	catch (ComputeException e) {
     		logger.error("Unable to update all entities to use new compressed file: "+outputPath);
     	}
 	}
+
+    private String getExtension(String filepath) {
+        int dot = filepath.indexOf('.');
+        if (dot>0) {
+            return filepath.substring(dot+1);
+        }
+        return "";
+    }
 }
--- model/src/org/janelia/it/jacs/model/entity/EntityConstants.java
+++ model/src/org/janelia/it/jacs/model/entity/EntityConstants.java
@@ -190,6 +190,7 @@ public class EntityConstants {
 
     public static final String ATTRIBUTE_MASK_IMAGE                          = "Mask Image";
     public static final String ATTRIBUTE_CHAN_IMAGE                          = "Chan Image";
+    public static final String ATTRIBUTE_LOSSY_IMAGE                         = "Lossy Image"; // Lossy-compressed version of the image
     
     public static final String ATTRIBUTE_ALIGNED_CONSOLIDATED_LABEL          = "Aligned Consolidated Label";
     
