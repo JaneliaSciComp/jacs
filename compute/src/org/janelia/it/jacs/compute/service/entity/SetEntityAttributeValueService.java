fb83ac84877aaa29e80125a1102e6bfc8d40b90d
Konrad Rokicki <rokickik@janelia.hhmi.org>
2015-05-22 09:44:09 -0400

    added sample compression options from the GUI

---
+++ compute/src/org/janelia/it/jacs/compute/service/entity/SetEntityAttributeValueService.java
@@ -0,0 +1,17 @@
+package org.janelia.it.jacs.compute.service.entity;
+
+/**
+ * Sets an attribute on an entity.
+ *   
+ * @author <a href="mailto:rokickik@janelia.hhmi.org">Konrad Rokicki</a>
+ */
+public class SetEntityAttributeValueService extends AbstractEntityService {
+
+    public void execute() throws Exception {
+    	Long entityId = data.getRequiredItemAsLong("ENTITY_ID");
+    	String attributeName = data.getRequiredItemAsString("ATTRIBUTE_NAME");
+    	String value = data.getRequiredItemAsString("ATTRIBUTE_VALUE");
+    	logger.info("Setting "+attributeName+" to "+value+" on entity "+entityId);
+    	entityBean.setOrUpdateValue(entityId, attributeName, value);
+    }
+}
