71e2a78fa6453a362f6f66fb3ad83c394a1c0bff
Konrad Rokicki <rokickik@janelia.hhmi.org>
2015-06-04 13:18:18 -0400

    ensure file is v3draw format before running splitColorChannels

--- compute/scripts/single_neuron/ImageTools/splitChannels.sh
+++ compute/scripts/single_neuron/ImageTools/splitChannels.sh
@@ -40,6 +40,10 @@ echo "Local input file: $INPUT_FILE"
 ensureUncompressedFile "$WORKING_DIR" "$INPUT_FILE" INPUT_FILE
 echo "Uncompressed input file: $INPUT_FILE"
 
+# Yang says that splitColorChannels works best with v3draw input, so here we ensure the file is v3draw
+ensureRawFile "$Vaa3D" "$WORKING_DIR" "$INPUT_FILE" INPUT_FILE
+echo "Raw input file: $INPUT_FILE"
+
 INPUT_NAME=`basename $INPUT_FILE`
 ln -s $INPUT_FILE $WORKING_DIR/$INPUT_NAME
 INPUT_FILE=$WORKING_DIR/$INPUT_NAME
