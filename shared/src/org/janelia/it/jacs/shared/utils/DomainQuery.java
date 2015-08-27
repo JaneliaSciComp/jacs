c686765aefa8656027564266aaf9935bffbde32f
porterbot <schauderd@janelia.hhmi.org>
2015-08-27 11:13:04 -0400

    forgot to add this to wrap post query objects

---
+++ shared/src/org/janelia/it/jacs/shared/utils/DomainQuery.java
@@ -0,0 +1,67 @@
+package org.janelia.it.jacs.shared.utils;
+
+import org.janelia.it.jacs.model.domain.Reference;
+import java.util.List;
+
+/**
+ * Created by schauderd on 8/24/15.
+ */
+public class DomainQuery {
+    String subjectKey;
+    List<Reference> references;
+    List<Long> objectIds;
+    String objectType;
+    String propertyName;
+    String propertyValue;
+
+    public List<Reference> getReferences() {
+        return references;
+    }
+
+    public void setReferences(List<Reference> references) {
+        this.references = references;
+    }
+
+    public String getSubjectKey() {
+        return subjectKey;
+    }
+
+    public void setSubjectKey(String subjectKey) {
+        this.subjectKey = subjectKey;
+    }
+
+
+    public List<Long> getObjectIds() {
+        return objectIds;
+    }
+
+    public void setObjectIds(List<Long> objectIds) {
+        this.objectIds = objectIds;
+    }
+
+
+    public String getObjectType() {
+        return objectType;
+    }
+
+    public void setObjectType(String objectType) {
+        this.objectType = objectType;
+    }
+
+
+    public String getPropertyName() {
+        return propertyName;
+    }
+
+    public void setPropertyName(String propertyName) {
+        this.propertyName = propertyName;
+    }
+
+    public String getPropertyValue() {
+        return propertyValue;
+    }
+
+    public void setPropertyValue(String propertyValue) {
+        this.propertyValue = propertyValue;
+    }
+}
