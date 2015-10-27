1e99f7b4ffe057c05e2cad90273b4bcbacead372
fosterl <fosterl@janalia.hhmi.org>
2015-10-27 09:41:32 -0400

    Saving writeback changes.

--- compute/src/org/janelia/it/jacs/compute/mbean/LargeVolumeSampleDiscovery.java
+++ compute/src/org/janelia/it/jacs/compute/mbean/LargeVolumeSampleDiscovery.java
@@ -1,22 +1,42 @@
 package org.janelia.it.jacs.compute.mbean;
 
+import org.janelia.it.jacs.compute.api.AnnotationBeanRemote;
 import org.janelia.it.jacs.compute.api.EJBFactory;
+import org.janelia.it.jacs.compute.api.EntityBeanRemote;
 import org.janelia.it.jacs.compute.api.TiledMicroscopeBeanRemote;
 import org.janelia.it.jacs.compute.largevolume.auto_discovery.SampleDiscovery;
+import org.janelia.it.jacs.model.entity.EntityActorPermission;
+import org.janelia.it.jacs.model.entity.EntityConstants;
+import org.janelia.it.jacs.model.entity.Entity;
+import org.janelia.it.jacs.model.user_data.tiledMicroscope.TmSample;
 
 import java.util.Set;
+import java.util.List;
 import java.io.File;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileOwnerAttributeView;
 /**
  * This should ultimately allow the user to invoke an auto-creation of samples for LVV.
  * Created by fosterl on 10/23/15.
  * @See TiledMicroscopeManager - which currently does not do much.  Should this method be in that instead?
  */
 public class LargeVolumeSampleDiscovery implements LargeVolumeSampleDiscoveryMBean {
+    public static final String SHARED_PERMISSION = "group:mouselight_common_user";
+
     @Override
     public void discoverSamples() {
         try {
+            // Need to find out whether samples already exist.
+            AnnotationBeanRemote annotationBean = EJBFactory.getRemoteAnnotationBean();
+//            List<Long> entityIds = annotationBean.getAllEntityIdsByType( EntityConstants.TYPE_3D_TILE_MICROSCOPE_SAMPLE );
+//            for (Long entityId : entityIds) {
+//                annotationBean.getEntitiesWithFilePath()
+//            }
+
             TiledMicroscopeBeanRemote timBean = EJBFactory.getRemoteTiledMicroscopeBean();
+            EntityBeanRemote entityBean = EJBFactory.getRemoteEntityBean();
             SampleDiscovery discovery = new SampleDiscovery();
             Set<File> sampleDirectories = discovery.discover();
             // NOTE: questions should be answered before going much further on this.
@@ -24,7 +44,32 @@ public class LargeVolumeSampleDiscovery implements LargeVolumeSampleDiscoveryMBe
             //  What if a sample on same directory already exists?
             //
             for (File sample: sampleDirectories) {
-                timBean.createTiledMicroscopeSample( null, sample.getName(), sample.getAbsolutePath() );
+                Path path = Paths.get(sample.getAbsolutePath());
+                FileOwnerAttributeView ownerAttributeView = Files.getFileAttributeView(path, FileOwnerAttributeView.class);
+                String fileLocation = sample.getAbsolutePath();
+                List<Entity> entities = annotationBean.getEntitiesWithFilePath(fileLocation);
+                boolean original = true;
+                for ( Entity entity: entities ) {
+                    if (entity.getEntityTypeName().equals(EntityConstants.TYPE_3D_TILE_MICROSCOPE_SAMPLE)) {
+                        Set<EntityActorPermission> permissions = entity.getEntityActorPermissions();
+                        for (EntityActorPermission permission: permissions) {
+                            if ( permission.getSubjectKey().equals(SHARED_PERMISSION)) {
+                                original = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if ( original ) {
+                    String ownerSubject = "user:" + ownerAttributeView.getOwner().getName();
+                    TmSample tmSample = timBean.createTiledMicroscopeSample(
+                            ownerSubject, sample.getName(), fileLocation
+                    );
+                    Long entityId = tmSample.getId();  // Happens to be the entity id of the wrapped entity.
+                    //Entity sampleEntity = entityBean.getEntityById(ownerSubject, entityId);
+                    entityBean.grantPermissions( ownerSubject, entityId, SHARED_PERMISSION, "r", true);
+                }
             }
         } catch (Exception ex) {
             ex.printStackTrace();
