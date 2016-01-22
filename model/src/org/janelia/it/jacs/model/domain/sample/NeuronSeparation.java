0f5f71e6fafc0243016dc9a59b34af3eef6fd97f
Konrad Rokicki <rokickik@janelia.hhmi.org>
2016-01-22 11:45:58 -0500

    removed SampleResult since parent pointers make it unnecessary

--- model/src/org/janelia/it/jacs/model/domain/sample/NeuronSeparation.java
+++ model/src/org/janelia/it/jacs/model/domain/sample/NeuronSeparation.java
@@ -3,6 +3,7 @@ package org.janelia.it.jacs.model.domain.sample;
 import org.janelia.it.jacs.model.domain.ReverseReference;
 import org.janelia.it.jacs.model.domain.support.SearchTraversal;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
 /**
@@ -14,24 +15,29 @@ public class NeuronSeparation extends PipelineResult {
 
     private String name;
     @SearchTraversal({Sample.class})
-    @JsonUnwrapped
     private ReverseReference fragments;
 
-    /* EVERYTHING BELOW IS AUTO-GENERATED */
+    @JsonProperty
     public String getName() {
         return name;
     }
 
+    @JsonProperty
     public void setName(String name) {
         this.name = name;
     }
 
+    @JsonProperty
+    @JsonUnwrapped
     public ReverseReference getFragmentsReference() {
         return fragments;
     }
 
+    @JsonProperty
+    @JsonUnwrapped
     public void setFragmentsReference(ReverseReference fragmentsReference) {
         this.fragments = fragmentsReference;
+        setId(fragments.getReferenceId());
     }
 
 }
