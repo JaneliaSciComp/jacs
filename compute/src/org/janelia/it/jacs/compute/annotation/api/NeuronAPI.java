e8b721580cfd27466b202e0521c75900fed061cf
lesfoster <fosterl@janelia.hhmi.org>
2016-01-25 18:01:42 -0500

    Simplistic skeletal version of the remove point, neuron and sample methods.  No database backing, just stubbed to emit log messages.

--- compute/src/org/janelia/it/jacs/compute/annotation/api/NeuronAPI.java
+++ compute/src/org/janelia/it/jacs/compute/annotation/api/NeuronAPI.java
@@ -8,6 +8,7 @@ package org.janelia.it.jacs.compute.annotation.api;
 
 //import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -90,6 +91,45 @@ public class NeuronAPI {
         }
     }
 
+    @DELETE
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/removePoint/{pointGUID}")
+    public Response removePoint(@PathParam("pointGUID") Long pointGUID) {
+        try {
+            removePointImpl(pointGUID);
+            return Response.ok(new GenericEntity<>("Point Removed", String.class)).build();
+        } catch (Exception ex) {
+            return errorResponse(ex);
+        }
+    }
+
+    @DELETE
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/removeNeuron/{neuronGUID}")
+    public Response removeNeuron(@PathParam("neuronGUID") Long pointGUID) {
+        try {
+            removeNeuronImpl(pointGUID);
+            return Response.ok(new GenericEntity<>("Neuron Removed", String.class)).build();
+        } catch (Exception ex) {
+            return errorResponse(ex);
+        }
+    }
+
+    @DELETE
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/removeSample/{sampleID}")
+    public Response removeSample(@PathParam("sampleID") Long sampleID) {
+        try {
+            removeSampleImpl(sampleID);
+            return Response.ok(new GenericEntity<>("Sample Removed", String.class)).build();
+        } catch (Exception ex) {
+            return errorResponse(ex);
+        }
+    }
+
     public Response errorResponse(Exception ex) {
         return Response.serverError()
                 .header("error-message", ex.getMessage())
@@ -98,5 +138,15 @@ public class NeuronAPI {
     
     private void addPointImpl(Long pointGUID, Long sampleID, Long neuronGUID, int x, int y, int z, int structureID, Long parentPointGUID) {
         log.info(String.format("Adding point: %d,%d,%d under UID %d w/ parent %d.  Adding to neuron %d.", x, y, z, pointGUID, parentPointGUID, neuronGUID));        
+    }
+    
+    private void removePointImpl(Long pointGUID) {
+        log.info("Removed point " + pointGUID);
+    }
+    private void removeNeuronImpl(Long neuronGUID) {
+        log.info("Removed neuron " + neuronGUID);
+    }
+    private void removeSampleImpl(Long sampleID) {
+        log.info("Removed sample " + sampleID);
     }
 }
