84cf107a8f0a84c804efd79f38f6b86b9abb6974
lesfoster <fosterl@janelia.hhmi.org>
2016-01-25 18:03:42 -0500

    Simplistic skeletal version of the remove point, neuron and sample methods.  No database backing, just stubbed to emit log messages.  Added comments for testing method.

--- compute/src/org/janelia/it/jacs/compute/annotation/api/NeuronAPI.java
+++ compute/src/org/janelia/it/jacs/compute/annotation/api/NeuronAPI.java
@@ -33,6 +33,10 @@ curl -H"Content-Type:application/json"  --request POST --data @${FILE} http://fo
 
 #
 curl -H"Content-Type:text/plain"  --request POST http://foster-ws:8180/${CONTEXT}/NeuronAPI/addPointQP/9999/?pointGUID=9999\&sampleID=8888\&neuronGUID=9996\&x=74000\&y=46000\&z=17000\&structureID=2\&parentPointGUID=9997
+
+curl -H"Content-Type:text/plain"  --request DELETE http://foster-ws:8180/rest-v1/NeuronAPI/removePoint/9999/
+curl -H"Content-Type:text/plain"  --request DELETE http://foster-ws:8180/rest-v1/NeuronAPI/removeNeuron/9997/
+curl -H"Content-Type:text/plain"  --request DELETE http://foster-ws:8180/rest-v1/NeuronAPI/removeSample/9996/
 */
 
 /**
