356094a5fe1144a98e2e97538747f2725611c403
fosterl <fosterl@janalia.hhmi.org>
2016-02-16 14:37:08 -0500

    Saving the JMX launcher.

--- compute/src/org/janelia/it/jacs/compute/mbean/TiledMicroscopeManager.java
+++ compute/src/org/janelia/it/jacs/compute/mbean/TiledMicroscopeManager.java
@@ -1,6 +1,23 @@
 package org.janelia.it.jacs.compute.mbean;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.List;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import org.apache.log4j.Logger;
+import org.janelia.it.jacs.compute.api.ComputeBeanRemote;
 import org.janelia.it.jacs.compute.api.EJBFactory;
+import org.janelia.it.jacs.compute.api.EntityBeanRemote;
+import org.janelia.it.jacs.model.entity.Entity;
+import org.janelia.it.jacs.model.entity.EntityConstants;
+import org.janelia.it.jacs.model.tasks.Event;
+import org.janelia.it.jacs.model.tasks.Task;
+import org.janelia.it.jacs.model.tasks.TaskParameter;
+import org.janelia.it.jacs.model.tasks.tiledMicroscope.TmNeuronPBUpdateTask;
+import org.janelia.it.jacs.model.tasks.utility.GenericTask;
+import org.janelia.it.jacs.model.user_data.Node;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,6 +36,35 @@ public class TiledMicroscopeManager implements TiledMicroscopeManagerMBean {
         }
     }
 
+    public void convertAllTmWorkspaceToProtobuf() {
+        try {
+            Logger logger = Logger.getLogger(TiledMicroscopeManager.class);
+            EntityBeanRemote entityBean = EJBFactory.getRemoteEntityBean();
+            if (entityBean == null) {
+                throw new RuntimeException("Failed to get Entity EJB");
+            }
+            List<Entity> workspaces = entityBean.getEntitiesByTypeName(null, EntityConstants.TYPE_TILE_MICROSCOPE_WORKSPACE);
+            
+            ComputeBeanRemote computeBean = EJBFactory.getRemoteComputeBean();
+            if (computeBean == null) {
+                throw new RuntimeException("Failed to get Compute EJB");
+            }
+
+            for (Entity workspace: workspaces) {
+                String workspaceIdStr = workspace.getId().toString();
+                HashSet<TaskParameter> taskParameters = new HashSet<>();
+                taskParameters.add(new TaskParameter(TmNeuronPBUpdateTask.PARAM_workspaceId, workspaceIdStr, null));
+                Task task = new GenericTask(new HashSet<Node>(), "group:mouselight", new ArrayList<Event>(),
+                        taskParameters, TmNeuronPBUpdateTask.PROCESS_NAME, TmNeuronPBUpdateTask.PROCESS_NAME);
+                task = computeBean.saveOrUpdateTask(task);
+                computeBean.submitJob(task.getJobName(), task.getObjectId());
+                logger.info("Submitted " + task.getJobName() + " for workspace " + workspace.getId());
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
     public void test() {
         System.out.println("TiledMicroscopeManager test() start");
 
