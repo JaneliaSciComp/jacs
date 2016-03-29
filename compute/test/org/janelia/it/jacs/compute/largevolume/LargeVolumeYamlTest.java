d87c0b10ab479920cbced92e4316654162def088
fosterl <fosterl@janalia.hhmi.org>
2016-03-29 10:30:24 -0400

    Adding flexibility to the reading of YAML files.  Now utilizes either homography or transform.

--- compute/test/org/janelia/it/jacs/compute/largevolume/LargeVolumeYamlTest.java
+++ compute/test/org/janelia/it/jacs/compute/largevolume/LargeVolumeYamlTest.java
@@ -15,15 +15,17 @@ import java.io.InputStream;
  * Created by fosterl on 9/24/14.
  */
 public class LargeVolumeYamlTest {
-    private static final String TILEBASE_YML = "tilebase.cache.yml";
     private static final String INFILE_RESOURCE = "/largevolume/2014-06-24-Descriptor-stitch1/";
-    public static final String TILING_PATH = "/tier2/mousebrainmicro/mousebrainmicro/data/2014-06-24/Tiling";
-    public static final String FIRST_TILE_RELATIVE_PATH = "/2014-07-05/02/02030";
+    private static final String OLD_TILING_PATH = "/tier2/mousebrainmicro/mousebrainmicro/data/2014-06-24/Tiling";
+    private static final String NEW_TILING_PATH = "/tier2/mousebrainmicro/mousebrainmicro/data/2015-06-19/Tiling";
+    private static final String OLD_FIRST_TILE_RELATIVE_PATH = "/2014-07-05/02/02030";
+    private static final String NEW_FIRST_TILE_RELATIVE_PATH = "/2015-06-22/00/00000";
     private InputStream testStream;
+    private InputStream modernYmlStream;
 
     @Before
     public void setUp() throws Exception {
-        if ( null == ( testStream = getTestFileStream() ) ) {
+        if ( null == ( testStream = TileWalkerTest.getTestFileStream() ) ) {
             String classpath = System.getProperty("java.class.path");
             String[] classpathMembers = classpath.split(":");
             StringBuilder bldr = new StringBuilder();
@@ -34,20 +36,39 @@ public class LargeVolumeYamlTest {
             }
             throw new Exception("Stream for " + INFILE_RESOURCE + " not found.  Working directory is " + System.getProperty("user.dir") + ", and interesting classpath is " + bldr.toString());
         }
+        if ( null == ( modernYmlStream = TileWalkerTest.getNewStyleTestFileStream() ) ) {
+            String classpath = System.getProperty("java.class.path");
+            String[] classpathMembers = classpath.split(":");
+            StringBuilder bldr = new StringBuilder();
+            for ( String member: classpathMembers ) {
+                if ( member.contains("compute")) {
+                    bldr.append(":").append(member);
+                }
+            }
+            throw new Exception("Stream for " + TileWalkerTest.NEWSTYLE_TILEBASE_YML + " under " + TileWalkerTest.JOHAN_FULL_DIR + " not found.  Working directory is " + System.getProperty("user.dir") + ", and interesting classpath is " + bldr.toString());
+        }
     }
 
     @Test
-    public void readFile() throws Exception {
-        //        Yaml yaml = new Yaml();
-        TileBase tileBase = new TileBaseReader().readTileBase( testStream );
+    public void readOldFile() throws Exception {
+        examineTileBase(testStream, OLD_TILING_PATH, OLD_FIRST_TILE_RELATIVE_PATH);
+    }
+
+    @Test
+    public void readModernFile() throws Exception {
+        examineTileBase(modernYmlStream, NEW_TILING_PATH, NEW_FIRST_TILE_RELATIVE_PATH);
+    }
+
+    @Test
+    public void checkYamlTolerance() throws Exception {
+        InputStream nsTestStream = TileWalkerTest.getKnitTestFileStream();
+        TileBase tileBase = new TileBaseReader().readTileBase(nsTestStream);
         Assert.assertNotNull("Null read", tileBase);
         String path = tileBase.getPath();
-        Assert.assertEquals("Unexpected path read.", path, TILING_PATH);
+        //Assert.assertEquals("Unexpected path read.", path, TILING_PATH);
         Assert.assertNotNull("Null tile set read.", tileBase.getTiles());
-        Tile tile = tileBase.getTiles().get( 0 );
-        Assert.assertNotNull("Null tile read.", tile );
-        String subPath = tile.getPath();
-        Assert.assertEquals("Tile sub path not as expected.", subPath, FIRST_TILE_RELATIVE_PATH);
+        Tile tile = tileBase.getTiles().get(0);
+        Assert.assertNotNull("Null tile read.", tile);
     }
 
     @After
@@ -55,9 +76,17 @@ public class LargeVolumeYamlTest {
 
     }
 
-    public static InputStream getTestFileStream() throws Exception {
-        File resourceFile = new File(TileWalkerTest.getBaseLocationFile(), TILEBASE_YML);
-        return new FileInputStream( resourceFile );
+    private void examineTileBase(InputStream inputStream, String tilingPath, String tileRelativePath) throws Exception {
+        //        Yaml yaml = new Yaml();
+        TileBase tileBase = new TileBaseReader().readTileBase( inputStream );
+        Assert.assertNotNull("Null read", tileBase);
+        String path = tileBase.getPath();
+        Assert.assertEquals("Unexpected path read.", path, tilingPath);
+        Assert.assertNotNull("Null tile set read.", tileBase.getTiles());
+        Tile tile = tileBase.getTiles().get( 0 );
+        Assert.assertNotNull("Null tile read.", tile );
+        String subPath = tile.getPath();
+        Assert.assertEquals("Tile sub path not as expected.", subPath, tileRelativePath);
     }
 
 }
