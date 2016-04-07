f2a474c6515587bc4f7016ff71099140520abd98
Konrad Rokicki <rokickik@janelia.hhmi.org>
2016-04-07 13:30:39 -0400

    added util function for making human readable strings

--- shared/src/org/janelia/it/jacs/shared/utils/StringUtils.java
+++ shared/src/org/janelia/it/jacs/shared/utils/StringUtils.java
@@ -261,4 +261,21 @@ public class StringUtils {
         return buffer.toString();
     }
     
+    /**
+     * Taken from https://stackoverflow.com/questions/2559759/how-do-i-convert-camelcase-into-human-readable-names-in-java
+     * 
+     * @param s
+     * @return
+     */
+    public static String splitCamelCase(String s) {
+        return s.replaceAll(
+           String.format("%s|%s|%s",
+              "(?<=[A-Z])(?=[A-Z][a-z])",
+              "(?<=[^A-Z])(?=[A-Z])",
+              "(?<=[A-Za-z])(?=[^A-Za-z])"
+           ),
+           " "
+        );
+     }
+    
 }
