f5d770c50163769b4e3392e9c723e0f4b3031e73
porterbot <schauderd@janelia.hhmi.org>
2016-08-03 15:28:21 -0400

    added new Task

---
+++ model/src/org/janelia/it/jacs/model/tasks/lineageClassifier/LineageClassifierTask.java
@@ -0,0 +1,65 @@
+
+package org.janelia.it.jacs.model.tasks.lineageClassifier;
+
+import java.util.List;
+import java.util.Set;
+
+import org.janelia.it.jacs.model.tasks.Event;
+import org.janelia.it.jacs.model.tasks.Task;
+import org.janelia.it.jacs.model.tasks.TaskParameter;
+import org.janelia.it.jacs.model.user_data.Node;
+import org.janelia.it.jacs.model.vo.ParameterException;
+import org.janelia.it.jacs.model.vo.ParameterVO;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Time: 3:03:13 PM
+ */
+public class LineageClassifierTask extends Task {
+    transient public static final String TASK_NAME = "LineageClassifier";
+    transient public static final String DISPLAY_NAME = "Lineage Classifier";
+
+    public static final String PARAM_SAMPLE = "sample id";
+
+    // Default values - default overrides
+
+    public LineageClassifierTask(Set<Node> inputNodes, String owner, List<Event> events, Set<TaskParameter> taskParameterSet) {
+        super(inputNodes, owner, events, taskParameterSet);
+        setDefaultValues();
+    }
+
+    public LineageClassifierTask() {
+        setDefaultValues();
+    }
+
+    private void setDefaultValues() {
+        this.taskName = TASK_NAME;
+    }
+
+    public ParameterVO getParameterVO(String key) throws ParameterException {
+        if (key == null)
+            return null;
+        String value = getParameter(key);
+        if (value == null)
+            return null;
+        // No match
+        return null;
+    }
+
+    public Long getSampleId() {
+        return getParameterAsLong(PARAM_SAMPLE);
+    }
+
+    public void setSampleId(Long sampleId) {
+        setParameterAsLong(PARAM_SAMPLE, sampleId);
+    }
+
+    public String getDisplayName() {
+        return DISPLAY_NAME;
+    }
+
+    public boolean isParameterRequired(String parameterKeyName) {
+        return true;
+    }
+
+}
